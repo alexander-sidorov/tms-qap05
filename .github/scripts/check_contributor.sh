1e95d2609cb5360af24ae3abefaff4eb4fd211b2
Alexander Sidorov <alexander@sidorov.dev>
2021-12-12 00:30:33 +0300

    fix ci

--- .github/scripts/check_contributor.sh
+++ .github/scripts/check_contributor.sh
@@ -91,9 +91,9 @@ for f in "$@"; do
   fi
 done
 
-echo "${pr}"
-echo "${name}"
-pr_ok=$(echo "${pr}" | grep "^${name} - Lesson \\d\+")
+gr="^${name} - Lesson \\\\d\+"
+echo "${gr}"
+pr_ok=$(echo "${pr}" | grep "${gr}")
 echo "${pr_ok}"
 if [[ -z "${pr_ok}" ]]; then
   echo "Malformed pull request title: '${pr}'"
