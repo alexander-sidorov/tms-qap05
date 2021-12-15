5a93ba841b4422b2b790807cb71bc4e17c296205
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-15 21:59:01 +0300

    test_saganovich.py

--- hw/maria_saganovich/test_saganovich.py
+++ hw/maria_saganovich/test_saganovich.py
@@ -1,2 +1,11 @@
 def test_example() -> None:
     assert True, "not True"
+    a = 2
+    b = 3
+    result = a if a > b else b
+    assert result
+    assert a + b
+    assert 100 / 2
+    assert "South" + "Korea"
+    assert "Busan" == "Busan"
+    assert a != 3
