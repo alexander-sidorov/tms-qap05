be34c2185f21108bd81124ccd7e3e970fee7ba2f
Valeriya Mavritskaya <valeriyamavritskaya@Valeriyas-MacBook-Air.local>
2021-12-15 20:59:55 +0300

    New asserts

--- hw/valeriya_mavritskaya/test_firstone.py
+++ hw/valeriya_mavritskaya/test_firstone.py
@@ -1,2 +1,8 @@
 def test_example() -> None:
     assert True, "not True"
+    assert 1 + 2 == 3
+    assert True == True
+    assert False == 0
+    assert "22" == True
+    assert "2" + "2" = "22"
+
