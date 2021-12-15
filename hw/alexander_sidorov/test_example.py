4b9694fbcd3b9b4fac6c173947736d069df39992
Alexander Sidorov <alexander@sidorov.dev>
2021-12-16 00:23:35 +0300

    xxx

--- hw/alexander_sidorov/test_example.py
+++ hw/alexander_sidorov/test_example.py
@@ -1,5 +1,7 @@
 def test_example() -> None:
     assert True, "not True"
     assert 0 ** 0 == 1
-    assert True + True == 2
+    assert (True + True) == 2
     assert "" + "" == ""
+
+    assert True == True  # noqa: E712,SIM300
