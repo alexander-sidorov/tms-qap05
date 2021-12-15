384386e5467d8d70c976acac276e0536a6246766
Tobkirill <tobkirill@gmail,.com>
2021-12-15 21:51:29 +0300

    new bool added

--- hw/kirill_tobolich/test_lesson2.py
+++ hw/kirill_tobolich/test_lesson2.py
@@ -1,5 +1,6 @@
 def test_example() -> None:
-    new_num = 1
+    new_bool = True
+    new_num = 123
     new_string = "jfkdsf"
-    assert new_num == True # noqa: E712,SIM300
+    assert new_bool is True
     assert new_num != new_string
