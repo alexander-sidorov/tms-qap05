f4244d7cd930d2f9be61d778709c1a66a7abe6cf
vadison90125 <vadison90125@gmail.com>
2021-12-15 21:57:35 +0300

    fix9

--- hw/vadim_maletski/test_lesson2.py
+++ hw/vadim_maletski/test_lesson2.py
@@ -1,8 +1,8 @@
 def test_example() -> None:
     assert True, "not True"
     assert 1 + 1 != 3, True
-    a = 2
-    b = 3
-    d = 5
-    c = a + b
-    assert c == d, True
+    num1 = 2
+    num2 = 3
+    num3 = 5
+    num4 = num1 + num2
+    assert num3 == num4, True
