85cd13483de925e07a8b2654caf43d92a1b9edd1
YaroslavBelaychuk <jaroslav.belaychuk@gmail.com>
2021-12-15 22:38:49 +0300

    bell13

--- hw/yaroslav_belaychuk/test_lesson2.py
+++ hw/yaroslav_belaychuk/test_lesson2.py
@@ -1,5 +1,6 @@
-num1 = 2
-num2 = 4
-num3 = 6
-assert num1 + num2 == num3, 'True'
- 
+def test_example() -> None:
+    assert True, "not True"
+    num1 = 2
+    num2 = 4
+    num3 = 6
+    assert num1 + num2 == num3, 'True'
