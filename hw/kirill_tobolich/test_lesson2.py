025e025da4a8127e0beeccc46c3c34b5c65d561f
Tobkirill <tobkirill@gmail,.com>
2021-12-15 21:17:41 +0300

    edited file

--- hw/kirill_tobolich/test_lesson2.py
+++ hw/kirill_tobolich/test_lesson2.py
@@ -1,4 +1,6 @@
 def test_example() -> None:
     new_num = 123
     new_string = "jfkdsf"
-    assert new_num == new_string, "Two variables are not equal"
+    assert new_num == True
+    assert new_string == True
+    assert new_num != new_string
