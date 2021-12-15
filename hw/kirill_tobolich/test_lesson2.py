b6b2eb45108cf287ece245b1d0ec61f025742b41
Tobkirill <tobkirill@gmail,.com>
2021-12-15 21:19:41 +0300

    edit assert

--- hw/kirill_tobolich/test_lesson2.py
+++ hw/kirill_tobolich/test_lesson2.py
@@ -1,6 +1,5 @@
 def test_example() -> None:
-    new_num = 123
+    new_num = 1
     new_string = "jfkdsf"
     assert new_num == True
-    assert new_string == True
     assert new_num != new_string
