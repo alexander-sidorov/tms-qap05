bd9936212f2bf6e442ef7d2b8a2faffc60a227e7
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-15 21:14:22 +0300

    test_lesson2

---
+++ hw/maria_saganovich/test_example.py
@@ -0,0 +1,12 @@
+def test_example() -> None:
+    assert True, "not True"
+    assert 100 + 2
+    assert 100 / 2
+    a = 3
+    b = 5
+    result = a if a > b else b
+    assert result
+    str1 = "Seoul"
+    str2 = "Paris"
+    assert str1 + str2
+	
