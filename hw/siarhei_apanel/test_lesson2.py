b4eeebfaa9005c2cb09d771ccefd337a10e24759
siarhei_apanel <aposha_18@mail.ru>
2021-12-15 22:04:45 +0300

    lesson2

--- hw/siarhei_apanel/test_lesson2.py
+++ hw/siarhei_apanel/test_lesson2.py
@@ -1,7 +1,6 @@
 a=7
-def yarik(a):
-    if a <= 6:
-        print('Yarik RainMan')
-    else:
-        print('Yarik CoolMan')
-yarik(a)
+
+if a <= 6:
+    print('Yarik RainMan')
+else:
+    print('Yarik CoolMan')
