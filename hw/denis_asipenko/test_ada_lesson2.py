52180d4a7cef1c5c1fa336d6fda9a8cd5b9c05fa
denisasipenko <asipenka.dzianis@gmail.com>
2021-12-16 16:28:26 +0300

    Denis Asipenko - Lesson2

---
+++ hw/denis_asipenko/test_ada_lesson2.py
@@ -0,0 +1,10 @@
+def test_example() -> None:
+    assert True, "not True"
+    assert 1 + 1 == 2
+    assert 3 * 3 == 9
+    assert 2 - 1 == 1
+    assert 3.9 + 0.1 == 4
+    assert 10 / 2 == 5
+    assert "" + "" == ""
+    assert (True + True) == 2
+    assert "denis" + "asipenko" == "denisasipenko"
