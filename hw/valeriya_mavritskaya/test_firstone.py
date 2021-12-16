d1683ea33b1bd5acb211c49d8f8a39bb9bf0f9b3
Valeriya Mavritskaya <valeriyamavritskaya@Valeriyas-MacBook-Air.local>
2021-12-16 13:39:56 +0300

    fixes

--- hw/valeriya_mavritskaya/test_firstone.py
+++ hw/valeriya_mavritskaya/test_firstone.py
@@ -2,6 +2,6 @@ def test_example() -> None:
     assert True, "not True"
     assert 1 + 2 == 3
     assert True is True
-    assert False == 0
+    assert False == 0  # noqa: SIM300,E712
     assert "22" == "22"
-    assert "2" + "2" == "22"
+    assert "2" + "2" == "22"  # noqa: SIM300
