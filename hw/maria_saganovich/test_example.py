668a21efd455a05ef28fbd4a9f6711e18f1d2d5a
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-16 12:45:13 +0300

    2 files are changed

--- hw/maria_saganovich/test_example.py
+++ hw/maria_saganovich/test_example.py
@@ -1,11 +1,13 @@
 def test_example() -> None:
     assert True, "not True"
-    assert (100 + 2 == 102)
-    num1  = 3
+    num1 = 3
     num2 = 5
-    result = num1 if num1 > num2 else num2
-    assert result != 1
+    num3 = 100
+    num4 = 2
     str1 = "South"
     str2 = "Korea"
+    assert (num3 + num4 == 102)
+    result = num1 if num1 > num2 else num2
+    assert result != 1
     assert (str1 + " " + str2 == "South Korea")
     assert str1[1:4] == "out"
--- hw/maria_saganovich/test_saganovich_lesson2.py
+++ hw/maria_saganovich/test_saganovich_lesson2.py
@@ -2,15 +2,16 @@ def test_example() -> None:
     assert True, "not True"
     num1 = 7
     num2 = 2
-    result = 7 % 2
-    assert result != 0
+    num3 = 3
     num4 = 10 / 3
+    num5 = 10
+    str1 = "Busan"
+    result = num1 % num2
+    assert result != 0
     assert num4 > 3
-    assert (100 / 2 == 50)
-    assert (10 ** 10 != 10)
-    assert "Busan" == "Busan"
+    assert (num5 ** 10 != 10)
+    assert str1 == "Busan"
     assert num1 != 3
     assert (num1 - num2 == 5)
-    num3 = 3
     num3 *=3
     assert num3 != 5
