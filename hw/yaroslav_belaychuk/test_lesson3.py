f9862defdea291aaa1122c5ece670641ab49ae16
YaroslavBelaychuk <jaroslav.belaychuk@gmail.com>
2021-12-17 22:19:10 +0300

    bell15

--- hw/yaroslav_belaychuk/test_lesson3.py
+++ hw/yaroslav_belaychuk/test_lesson3.py
@@ -1,6 +1,11 @@
 def f() -> None:
-    4 # noqa: B018
+    4  # noqa: B018
 
 
 def g() -> int:
     return 4
+
+
+def test_func() -> None:
+    assert f() is None  # type: ignore
+    assert g() == 4
