49cf9146b194b8a015f142d2523e55db512df34a
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-17 22:09:18 +0300

    commit

--- hw/maria_saganovich/saganovich_lesson3.py
+++ hw/maria_saganovich/saganovich_lesson3.py
@@ -1,5 +1,9 @@
-def test_1():
+def test_1() -> None:
     4 # noqa: B018
 
-def f(a, b):
-    return a + b
+def g() -> int:
+    return 4
+
+def f() -> None:
+    assert f() is None #type: ignore
+    assert g() == 4
