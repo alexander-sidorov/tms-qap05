c4cd384f10240838fb3777ae34dc852d12609c3e
siarhei_apanel <aposha_18@mail.ru>
2021-12-19 20:45:17 +0300

    HW

--- hw/siarhei_apanel/test_hw.py
+++ hw/siarhei_apanel/test_hw.py
@@ -1,17 +1,17 @@
-def func(a1: float, back: float, cill: float) -> str:
+def func(a1: float, back: float, cill: float) -> tuple:
     if a1 == 0:
-        return "None"
+        return (None)
     disk = back ** 2 - 4 * a1 * cill
     sqrt = disk ** (0.5)
     if disk > 0:
         x1 = (-back + sqrt) / 2 * a1
         x2 = (-back - sqrt) / 2 * a1
-        return str(x1), str(x2)
+        return (x1, x2)
     elif disk == 0:
         xone = (-back + sqrt) / 2 * a1
-        return str(xone)
+        return (xone)
     else:
-        return "None"
+        return (None)
 
 def test() -> None:
     func(1, 2, 1) == -1.0
