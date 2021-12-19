a07bc1d44523cdc491679c139f4c2ec2da28abb3
siarhei_apanel <aposha_18@mail.ru>
2021-12-19 19:48:04 +0300

    HW

---
+++ hw/siarhei_apanel/test_hw.py
@@ -0,0 +1,14 @@
+def f(a1, back, call):
+    if a1 == 0:
+        return "Нет корней"
+    disk = back ** 2 - 4 * a1 * call
+    sqrt = disk ** (0.5)
+    if disk > 0:
+        x1 = (-back + sqrt) / 2 * a1
+        x2 = (-back - sqrt) / 2 * a1
+        return x1, x2
+    elif disk == 0:
+        xone = (-back + sqrt) / 2 * a1
+        return xone
+    else:
+        return "Нет корней"
