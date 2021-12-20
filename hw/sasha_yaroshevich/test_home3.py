4273236ea8d92759931dcf3c7bc14c1656638bc9
Sasha <Pristigesasha@gmail.com>
2021-12-20 20:21:50 +0300

    Sasha Yaroshevich - Lesson 03

---
+++ hw/sasha_yaroshevich/test_home3.py
@@ -0,0 +1,26 @@
+def func() -> True:
+    return True
+
+
+def funct() -> False:
+    return False
+
+
+def fun() -> None:
+    pass
+
+
+def fu() -> int:
+    return -4
+
+
+def f() -> str:
+    return ""
+
+
+def test_home3() -> None:
+    assert func() is True
+    assert funct() is False
+    assert fun() is None  # type: ignore
+    assert fu() < 0
+    assert f() == ""
