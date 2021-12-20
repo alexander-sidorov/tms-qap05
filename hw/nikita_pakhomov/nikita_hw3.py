8e34c7d43a77dbf04157d9256e90de87d308715a
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-21 00:00:22 +0300

    dell f

--- hw/nikita_pakhomov/nikita_hw3.py
+++ hw/nikita_pakhomov/nikita_hw3.py
@@ -16,20 +16,3 @@ def d() -> int:
 
 def e() -> str:
     return ""
-
-
-def solve(a, b, c):
-    from math import sqrt, pow, pi, floor, ceil
-
-    d = (b ** 2) - 4 * a * c
-
-    if d < 0:
-        return "Нет корней"
-    elif d == 0:
-        v = -b / (2 * a)
-        return v, v
-    else:
-        x1 = ((-b) - (sqrt(d))) / (2 * a)
-        x2 = ((-b) + (sqrt(d))) / (2 * a)
-
-        return (min(x1, x2)), (max(x1, x2))
