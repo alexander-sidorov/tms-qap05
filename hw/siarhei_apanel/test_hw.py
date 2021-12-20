dbaf2a929a2147e14b549c2d2d6a69db5d300cb6
siarhei_apanel <aposha_18@mail.ru>
2021-12-20 12:20:01 +0300

    HW

--- hw/siarhei_apanel/test_hw.py
+++ hw/siarhei_apanel/test_hw.py
@@ -3,14 +3,21 @@ def func(a1: float, back: float, cill: float) -> list:
         return [None, None]
     disk = back ** 2 - 4 * a1 * cill
     sqrt = disk ** (0.5)
+
     if disk >= 0:
         x1 = (-back + sqrt) / 2 * a1
         x2 = (-back - sqrt) / 2 * a1
         return [x1, x2]
     else:
-        return [None, None]
+        x1 = (-back + sqrt) / 2 * a1
+        x2 = (-back - sqrt) / 2 * a1
+
+        return [x1, x2]
 
 
 def test() -> None:
     assert func(1, 2, 1) == [-1.0, -1.0]
-    assert func(1, 1, 1) == [None, None]
+    assert func(1, 1, 1) == [
+        (-0.49999999999999994 + 0.8660254037844386j),
+        (-0.5 - 0.8660254037844386j),
+    ]
