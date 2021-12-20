a39a693404468992e85f97796798250e2b4cacda
denisasipenko <asipenka.dzianis@gmail.com>
2021-12-21 01:34:54 +0300

    Denis Asipenko - Lesson3(hw.edt.)

---
+++ hw/denis_asipenko/test_da_hw.py
@@ -0,0 +1,26 @@
+def d() -> bool:
+    return True
+
+
+def e() -> bool:
+    return False
+
+
+def n() -> None:
+    return None
+
+
+def i() -> int:
+    return -3
+
+
+def s() -> str:
+    return ""
+
+
+def test_denis() -> None:
+    assert d() is True
+    assert e() is False
+    assert n() is None  # type: ignore
+    assert i() < 0
+    assert s() == ""
