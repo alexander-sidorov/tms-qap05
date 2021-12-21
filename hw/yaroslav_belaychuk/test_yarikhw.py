f8f7b8a7e48ca5ee4ae2187652400c4f48eac8ad
YaroslavBelaychuk <jaroslav.belaychuk@gmail.com>
2021-12-21 13:56:36 +0300

    bell17

--- hw/yaroslav_belaychuk/test_yarikhw.py
+++ hw/yaroslav_belaychuk/test_yarikhw.py
@@ -4,7 +4,12 @@ def fnc(num1: float, num2: float, num3: float) -> list:
     x1 = -num2 + kordsk / 2 * num1
     x2 = -num2 - kordsk / 2 * num1
     return [x1, x2]
+
+
 def test_f() -> None:
-    assert fnc(1, 4, 9) == [(-4+2.23606797749979j), (-4-2.23606797749979j)]
-    assert fnc(1, 1, 1) == [(-1+0.8660254037844386j), (-1-0.8660254037844386j)]
+    assert fnc(1, 4, 9) == [(-4 + 2.23606797749979j), (-4 - 2.23606797749979j)]
+    assert fnc(1, 1, 1) == [
+        (-1 + 0.8660254037844386j),
+        (-1 - 0.8660254037844386j),
+    ]
     assert fnc(1, 2, 1) == [-2.0, -2.0]
