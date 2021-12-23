2dbae3edf9f84eda08c62ca8dbc41753c64d4256
Sasha <Pristigesasha@gmail.com>
2021-12-23 21:25:13 +0300

    Sasha Yaroshevich - Lesson 04

--- hw/sasha_yaroshevich/home_work4.py
+++ hw/sasha_yaroshevich/home_work4.py
@@ -1,1 +1,8 @@
+from random import random  # noqa: DUO102
 
+
+def func() -> str:
+    if random() < 0.5:  # noqa: DUO102, S311
+        return "Оно и видно!"
+    else:
+        return "А могли бы и знать!"
---
+++ hw/sasha_yaroshevich/test_lessonhome4.py
@@ -0,0 +1,5 @@
+from hw.sasha_yaroshevich.home_work4 import func
+
+
+def test_lesson4() -> str:
+    assert func() == "Оно и видно!" or "А могли бы и знать!"
