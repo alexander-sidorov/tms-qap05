4218542bbcf7c264d55c5adf42cefafd91d713e7
vadison90125 <vadison90125@gmail.com>
2021-12-23 15:40:26 +0300

    func answer + test

---
+++ hw/vadim_maletski/test_hw4func.py
@@ -0,0 +1,12 @@
+from random import random
+
+
+def func_answer() -> int:
+
+    ran = random()
+    if ran < 0.5:
+        r1 = 1
+        return r1
+    else:
+        r2 = 0
+        return r2
---
+++ hw/vadim_maletski/test_hw4test.py
@@ -0,0 +1,5 @@
+from hw.vadim_maletski.test_hw4func import func_answer
+
+
+def test() -> None:
+    assert func_answer() == 0 or 1
