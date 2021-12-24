d20615cd6902d10b67d3a3adae1e925bfb3b87fe
Zyola <ded-undead@mail.ru>
2021-12-24 22:08:27 +0300

    ttttyyy

--- hw/nikita_pakhomov/test_lesson5.py
+++ hw/nikita_pakhomov/test_lesson5.py
@@ -1,7 +1,10 @@
+from hw.nikita_pakhomov.nlesson3.lesson5 import aggression
 
-def test_aggression()-> None:
-    assert aggression (True) == 'hhh'
-    assert aggression (False) == 'jj'
 
-if __name__ == '__main__':
+def test_aggression() -> None:
+    assert aggression(True) == "hhh"
+    assert aggression(False) == "ggg"
+
+
+if __name__ == "__main__":
     aggression(True)
