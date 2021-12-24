1990b808b120e13d429002f2e22f32a8744cdc48
Apanel Siarhei <95868187+APOSHAml@users.noreply.github.com>
2021-12-24 11:17:46 +0300

    Siarhei Apanel - Lesson 04 HW (#91)
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * HW
    
    * Hw
    
    * HW
    
    * HW
    
    * THE_FINAL
    
    * Lesson4
    
    * refact
    
    * refact
    
    * refact
    
    * refact
    
    * refact
    
    * HW2
    
    * HW2
    
    * HW2

--- hw/siarhei_apanel/refakt.py
+++ hw/siarhei_apanel/refakt.py
@@ -1,7 +1,6 @@
 def func(a1: float, back: float, cill: float) -> list:
     disk = back ** 2 - 4 * a1 * cill
     sqrt = disk ** 0.5
-
     x1 = (-back + sqrt) / 2 * a1
     x2 = (-back - sqrt) / 2 * a1
     return [x1, x2]
@@ -13,3 +12,12 @@ def far() -> None:
 
 def gar() -> int:
     return 4
+
+
+def vopros(answer: str) -> str:
+    if answer == "Да":
+        return "Оно и видно!"
+    elif answer == "Нет":
+        return "А могли бы и знать!"
+    else:
+        return "Несоответствующее значение"
