f1b241ba6de718c737020ab30615ba99a1d20b41
Zyola <ded-undead@mail.ru>
2021-12-24 21:57:14 +0300

    tttt

---
+++ hw/nikita_pakhomov/nlesson3/lesson5.py
@@ -0,0 +1,10 @@
+def aggression(know: bool) -> str:
+    if know:
+        r = 'uuu'
+    else:
+        r = 'ggg'
+    return
+
+if __name__ == '__main__':
+    print(aggression(True))
+    print(aggression(False))
---
+++ hw/nikita_pakhomov/test_lesson5.py
@@ -0,0 +1,7 @@
+
+def test_aggression()-> None:
+    assert aggression (True) == 'hhh'
+    assert aggression (False) == 'jj'
+
+if __name__ == '__main__':
+    aggression(True)
