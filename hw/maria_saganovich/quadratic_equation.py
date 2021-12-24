879e00605b65dc65d033116dc0e37f58fac5631a
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-24 17:20:02 +0300

    some_commit

---
+++ hw/maria_saganovich/quadratic_equation.py
@@ -0,0 +1,40 @@
+import math
+from typing import Any
+
+
+def quadratic_equation() -> Any:
+    x1: float
+    x2: float
+    x3: float
+
+    print("Enter the coefficients for the equation: ")  # noqa: T001
+
+    print("ax^2 + bx + c = 0:")  # noqa: T001
+    a1 = float(input("a = "))
+
+    if a1 == 0:
+        return "'a' couldn't be = 0"
+
+    b1 = float(input("b = "))
+    c1 = float(input("c = "))
+
+    discriminant = b1 ** 2 - 4 * a1 * c1
+    print("Discriminant: D = %.1f" % discriminant)  # noqa: S001, MOD001, T001
+
+    if discriminant > 0:
+        x1 = (-b1 + math.sqrt(discriminant)) / (2 * a1)
+        x2 = (-b1 - math.sqrt(discriminant)) / (2 * a1)
+        print(  # noqa: S001, MOD001, T001
+            "We got the next roots: x1 = %.1f, x2 = %.1f"  # noqa: S001, MOD001, T001
+            % (x1, x2)  # noqa: S001, MOD001, T001
+        )
+        return [x1, x2]
+    elif discriminant == 0:
+        x3 = -b1 / (2 * a1)
+        print(  # noqa: S001, MOD001, T001
+            "We have the next root:\nx = %.1f" % x3  # noqa: S001, MOD001, T001
+        )  # noqa: S001, MOD001, T001
+        return x3
+    else:
+        print("We have the next roots: \nNo roots")  # noqa: T001
+        return False
---
+++ hw/maria_saganovich/test_quadratic.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import patch
+
+from hw.maria_saganovich.quadratic_equation import quadratic_equation
+
+
+class TestQuadraticEquation(unittest.TestCase):
+    @patch("builtins.input", side_effect=[1, 2, 1])
+    def test_quadratic_one_root(self, mock_input) -> None:
+        result = quadratic_equation()
+        self.assertEqual(result, -1.0)
+
+    @patch("builtins.input", side_effect=[1, 2, -5])
+    def test_quadratic_two_roots(self, mock_input) -> None:
+        result = quadratic_equation()
+        self.assertEqual(result, [1.4494897427831779, -3.449489742783178])
+
+    @patch("builtins.input", side_effect=[0, 2, -5])
+    def test_quadratic_no_roots(self, mock_input) -> None:
+        result = quadratic_equation()
+        self.assertEqual(result, "'a' couldn't be = 0")
