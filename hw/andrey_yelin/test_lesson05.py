572233a97c3b20a03bf80952c69a59a1d382b425
zyola <ded-undead@mail.ru>
2021-12-24 20:49:30 +0300

    make some changes

--- hw/andrey_yelin/test_lesson05.py
+++ hw/andrey_yelin/test_lesson05.py
@@ -4,3 +4,9 @@ from hw.andrey_yelin.yelin_lesson05 import aggression
 def test_aggression() -> None:
     assert aggression(True) == "оно и видно"
     assert aggression(False) == "а мог бы и знать"
+
+
+if __name__ == "__main__":
+    agression(True)
+else:
+    aggression(False)
