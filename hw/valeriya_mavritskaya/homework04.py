be8254a9e015104f56c426a0bb7df4cff1f88fd0
valeriyamavritskaya <valerymavritskaya@gmail.com>
2021-12-24 20:48:51 +0300

    Valeriya Mavritskaya - Lesson 05

--- hw/valeriya_mavritskaya/homework04.py
+++ hw/valeriya_mavritskaya/homework04.py
@@ -1,12 +1,12 @@
 def aggression(know: bool) -> str:
     if know:
-        result = "оно и видно"
+        result = "оно и видно!"
     else:
-        result = "а мог бы и знать"
+        result = "а мог бы и знать!"
 
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     aggression(True)
     aggression(False)
--- hw/valeriya_mavritskaya/test_homework04.py
+++ hw/valeriya_mavritskaya/test_homework04.py
@@ -2,5 +2,5 @@ from hw.valeriya_mavritskaya.homework04 import aggression
 
 
 def test_homework04() -> None:
-    assert aggression(True) == "оно и видно"
-    assert aggression(False) == "а мог бы и знать"
+    assert aggression(True) == "оно и видно!"
+    assert aggression(False) == "а мог бы и знать!"
