f9747999f76e9326a0536015bca86cdd1ad0d0bb
YaroslavBelaychuk <jaroslav.belaychuk@gmail.com>
2021-12-24 20:33:54 +0300

    bell20

--- hw/yaroslav_belaychuk/test_lesson5.py
+++ hw/yaroslav_belaychuk/test_lesson5.py
@@ -1,1 +1,10 @@
+def aggression(know: bool) -> str:
+    if know:
+        return "но и видно"
+    else:
+        return " мог бы и знать"
 
+
+def test_f() -> None:
+    assert aggression(True) == "но и видно"
+    assert aggression(False) == " мог бы и знать"
