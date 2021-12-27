9647a701a3ffce674488874e499e7a94341fdddb
siarhei_apanel <aposha_18@mail.ru>
2021-12-27 22:36:03 +0300

    Lesson05HW

--- hw/siarhei_apanel/test.py
+++ hw/siarhei_apanel/test.py
@@ -7,15 +7,14 @@ from hw.siarhei_apanel.refakt import zaglav
 
 
 def test_example() -> None:
+    cod = "VD DzgS PwFl DzgS SDK ZFz HD"
+    key = "FTZHfrcwtoRgQzDaspdlKiPvSYLekVCqhJbyEnmMBAOIxuXjWUNG"
     assert korteg([1, 3, 5, 8]) == (1, 8)
     assert newwords("asfN dB12") == "dB12 asfN"
     assert srez([2, 14, "b", "h", "e"], "r") == [2, 14, "b", "h", "e", "r"]
     assert stroki("a4N67;i", "V") == "aV4VNV6V7V;ViV"
     assert zaglav("HI nub  hI   PrO") == "Hi Nub  Hi   Pro"
     assert (
-        krypto(
-            "VD DzgS PwFl DzgS SDK ZFz HD",
-            "FTZHfrcwtoRgQzDaspdlKiPvSYLekVCqhJbyEnmMBAOIxuXjWUNG",
-        )
+        krypto(cod, key)
         == "D o   o n l y   w h a t   o n l y   y o u   c a n   d o"
-    )
+    )  # noqa: W503
