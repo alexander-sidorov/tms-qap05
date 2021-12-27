1f123511332385c6ff6ca0def100cbc56ce06411
siarhei_apanel <aposha_18@mail.ru>
2021-12-27 22:07:36 +0300

    Lesson05HW

--- hw/siarhei_apanel/refakt.py
+++ hw/siarhei_apanel/refakt.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def func(a1: float, back: float, cill: float) -> list:
     disk = back ** 2 - 4 * a1 * cill
     sqrt = disk ** 0.5
@@ -19,6 +22,40 @@ def aggression(known: bool) -> str:
         return "Оно и видно!"
     else:
         return "А могли бы и знать!"
+
+
+def korteg(spisok: list) -> tuple:
+    return (spisok[0], spisok[-1])
+
+
+def newwords(words: str) -> str:
+    words1 = words.split()
+    return words1[1] + " " + words1[0]
+
+
+def srez(spisk: list, another: Any) -> list:
+    spisk.append(another)
+    return spisk
+
+
+def stroki(stroka: str, stroka2: str) -> str:
+    return "".join(lit + stroka2 for lit in stroka)
+
+
+def zaglav(stroka: str) -> str:
+    return stroka.title()
+
+
+def krypto(cod: str, key: str) -> str:
+    alphavit = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ind = []
+    for i1 in cod:
+        if i1 == " ":
+            ind.append(i1)
+        else:
+            ind.append(alphavit[key.find(i1)])
+
+    return " ".join(ind)
 
 
 if __name__ == "__main__":
