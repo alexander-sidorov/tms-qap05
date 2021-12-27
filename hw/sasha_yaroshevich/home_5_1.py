e5e75322dad0e76633f3d670dd5c97dcb483f2fc
Sasha <Pristigesasha@gmail.com>
2021-12-27 21:16:18 +0300

    Sasha Yaroshevich - Lesson 05

---
+++ hw/sasha_yaroshevich/home_5_1.py
@@ -0,0 +1,14 @@
+def funct(sort_collection):
+    return (sort_collection[0], sort_collection[-1])
+
+def func(stroka_two_words):
+    a = stroka_two_words.split(',')
+    b = [a[-1], a[0]]
+    return ', '.join(b)
+
+def fun(sort_coll, p2):
+    return sort_coll[0:sort_coll.index(p2)]
+
+def fu(stroka, stroka_one):
+    new = list(stroka)
+    s = 'stroka_one'.join(new)
