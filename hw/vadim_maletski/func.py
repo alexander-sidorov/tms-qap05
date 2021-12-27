815e6763fa7d24e67c0e1f83316774e6ee045732
vadison90125 <vadison90125@gmail.com>
2021-12-27 10:26:10 +0300

    fix2

--- hw/vadim_maletski/func.py
+++ hw/vadim_maletski/func.py
@@ -1,19 +1,10 @@
 def func_answer(ans: bool) -> str:
-<<<<<<< HEAD
-
-    if ans:
-        return "Оно и видно"
-    else:
-        return "А могли бы знать"
-
-=======
 
     if ans:
         return "Оно и видно "
     else:
         return "А могли бы знать "
 
->>>>>>> 811b5d63975d5186450efe813ec06b4aea2a22c8
 
 if __name__ == "__main__":
     func_answer(True)
