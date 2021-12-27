6f37a4836be6d2a170cd3a1b74e8c341763ddf07
vadison90125 <vadison90125@gmail.com>
2021-12-27 10:22:57 +0300

    fix

--- hw/vadim_maletski/test_hw4test.py
+++ hw/vadim_maletski/test_hw4test.py
@@ -2,5 +2,10 @@ from hw.vadim_maletski.func import func_answer
 
 
 def test() -> None:
+<<<<<<< HEAD
     assert func_answer(True) == "Оно и видно"
     assert func_answer(False) == "А могли бы знать"
+=======
+    assert func_answer(True) == "Оно и видно "
+    assert func_answer(False) == "А могли бы знать "
+>>>>>>> 811b5d63975d5186450efe813ec06b4aea2a22c8
