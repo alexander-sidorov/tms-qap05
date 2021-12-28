b20bfb35974eb470e004fef3cd9ffe4980811306
Sasha <Pristigesasha@gmail.com>
2021-12-28 23:36:46 +0300

    Sasha Yaroshevich - Lesson 05

--- hw/sasha_yaroshevich/home_5_1.py
+++ hw/sasha_yaroshevich/home_5_1.py
@@ -1,23 +1,26 @@
-def funct(sort_collection) -> tuple:
+from typing import Any
+
+
+def funct(sort_collection: Any) -> tuple:
     return (sort_collection[0], sort_collection[-1])
 
 
-def func(stroka_two_words) -> str:
+def func(stroka_two_words: str) -> str:
     new_a = stroka_two_words.split(" ")
     new_b = [new_a[-1], new_a[0]]
     return " ".join(new_b)
 
 
-def fun(sort_coll, p2) -> list:
+def fun(sort_coll: Any, p2: str) -> list:
     limit = sort_coll.index(p2)
     return sort_coll[0 : limit + 1]  # noqa: E203
 
 
-def fu(stroka, stroka_one) -> str:
+def fu(stroka: str, stroka_one: str) -> str:
     new_stroka = list(stroka)
     stroka_s = stroka_one.join(new_stroka)
     return stroka_s + stroka_one
 
 
-def f(stroka_pr) -> str:
+def f(stroka_pr: str) -> str:
     return stroka_pr.title()
