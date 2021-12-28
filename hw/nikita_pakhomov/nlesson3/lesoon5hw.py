6e7109db4f651fa2540aee688e00fd8b8b2d2b85
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-28 21:05:54 +0300

    hw5 function2

--- hw/nikita_pakhomov/nlesson3/lesoon5hw.py
+++ hw/nikita_pakhomov/nlesson3/lesoon5hw.py
@@ -1,24 +1,6 @@
-from typing import Any
-
-
 def strok(abc: str) -> str:
     prabel = abc.find(" ")
     strok1 = abc[:prabel]
-    strok2 = abc[prabel + 1 :]  # noqa
+    strok2 = abc[prabel:]
     strok3 = strok2 + abc[prabel] + strok1
     return strok3
-
-
-def lwl1(p1: tuple) -> tuple:
-    krt = (p1[0], p1[-1])
-    return krt
-
-
-def lwl3(collection: Any, object_1: Any) -> Any:
-    i = collection.index(object_1)
-    return collection[: i + 1]
-
-
-def lwl5(p1: str) -> str:
-    otvet = p1.title()
-    return otvet
