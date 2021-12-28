7a07f7eb4f80ec8f231e59d1472a136719345b6a
valeriyamavritskaya <valerymavritskaya@gmail.com>
2021-12-28 22:37:30 +0300

    Valeriya Mavritskaya - Lesson 05

---
+++ hw/valeriya_mavritskaya/lesson05_functions.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+def lvl_1(collection: Any) -> tuple:
+    return collection[0], collection[-1]
+
+
+def lvl_2(phrase: str) -> str:
+    exchange = phrase.rsplit()
+    return f"{exchange[1]} {exchange[0]}"
+
+
+def lvl_3(collection3: Any, object3: Any) -> Any:
+    new_collection = collection3.index(object3)
+    return collection3[: new_collection + 1]
+
+
+def lvl_4(some_string: str, symbol: str) -> str:
+    return "".join(x + symbol for x in some_string)
+
+
+def lvl_5(string_to_be_capitalized: str) -> str:
+    return string_to_be_capitalized.title()
---
+++ hw/valeriya_mavritskaya/test_lesson05.py
@@ -0,0 +1,32 @@
+from hw.valeriya_mavritskaya.lesson05_functions import lvl_1
+from hw.valeriya_mavritskaya.lesson05_functions import lvl_2
+from hw.valeriya_mavritskaya.lesson05_functions import lvl_3
+from hw.valeriya_mavritskaya.lesson05_functions import lvl_4
+from hw.valeriya_mavritskaya.lesson05_functions import lvl_5
+
+
+def test_lvl_1() -> None:
+    collection = [1, 2, 3, 66, 4, 5, 6]
+    assert lvl_1(collection) == (1, 6)
+
+
+def test_lvl_2() -> None:
+    phrase = "two words"
+    assert lvl_2(phrase) == "words two"
+
+
+def test_lvl_3() -> None:
+    collection3 = (1, 2, 3, 66, 4, 5, 6)
+    object3 = 66
+    assert lvl_3(collection3, object3) == (1, 2, 3, 66)
+
+
+def test_lvl_4() -> None:
+    some_string = "val"
+    symbol = "*"
+    assert lvl_4(some_string, symbol) == "v*a*l*"
+
+
+def test_lvl_5() -> None:
+    string_to_be_capitalized = "nice to meet you"
+    assert lvl_5(string_to_be_capitalized) == "Nice To Meet You"
