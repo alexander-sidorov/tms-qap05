de6f898424dcd71914d9b04ca6776fb6c687b17d
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-28 21:17:37 +0300

    hwtests

--- hw/nikita_pakhomov/test_hw5.py
+++ hw/nikita_pakhomov/test_hw5.py
@@ -1,1 +1,9 @@
 from hw.nikita_pakhomov.nlesson3.lesoon5hw import strok
+
+
+def test_strok() -> None:
+    assert strok("111 222") == "222 111"
+
+
+if __name__ == "__main__":
+    strok("111 222")
