733ef5c702e8e41c9d3a998048f6067649e8840e
siarhei_apanel <aposha_18@mail.ru>
2021-12-28 22:54:18 +0300

    Lesson05HW

--- hw/siarhei_apanel/test.py
+++ hw/siarhei_apanel/test.py
@@ -6,15 +6,22 @@ from hw.siarhei_apanel.refakt import stroki
 from hw.siarhei_apanel.refakt import zaglav
 
 
-def test_example() -> None:
+def test_example() -> None:  # noqa: W503
     cod = "VD DzgS PwFl DzgS SDK ZFz HD"
-    key = "FTZHfrcwtoRgQzDaspdlKiPvSYLekVCqhJbyEnmMBAOIxuXjWUNG"
-    assert korteg([1, 3, 5, 8]) == (1, 8)
+    key = "FTZHfrcwtoRgQzDaspdlKiPvSYLekVCqhJbyEnmMBAOIxuXjWUNG"  # noqa: E501
+    assert korteg([1, 3.0, "re", 8]) == (1, 8)
     assert newwords("asfN dB12") == "dB12 asfN"
-    assert srez([2, 14, "b", "h", "e"], "r") == [2, 14, "b", "h", "e", "r"]
+    assert srez([2.0, 14, "b", ["h"], "e"], "r") == [
+        2.0,
+        14,
+        "b",
+        ["h"],
+        "e",
+        "r",
+    ]
     assert stroki("a4N67;i", "V") == "aV4VNV6V7V;ViV"
     assert zaglav("HI nub  hI   PrO") == "Hi Nub  Hi   Pro"
     assert (
         krypto(cod, key)
-        == "D o   o n l y   w h a t   o n l y   y o u   c a n   d o"
-    )  # noqa: W503
+        == "D o   o n l y   w h a t   o n l y   y o u   c a n   d o"  # noqa: W503, E501
+    )  # noqa: W503, E501
