5a5c02bf1f4f2af90766b1543bd3ca8f474304d1
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-28 21:05:54 +0300

    hw5 function2

---
+++ hw/nikita_pakhomov/nlesson3/lesoon5hw.py
@@ -0,0 +1,6 @@
+def strok(abc: str) -> str:
+    prabel = abc.find(" ")
+    strok1 = abc[:prabel]
+    strok2 = abc[prabel:]
+    strok3 = strok2 + abc[prabel] + strok1
+    return strok3
---
+++ hw/nikita_pakhomov/test_hw5.py
@@ -0,0 +1,1 @@
+from hw.nikita_pakhomov.nlesson3.lesoon5hw import strok
