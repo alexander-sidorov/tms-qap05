eef16e4f91fb07579ead066502c4857680c43adf
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-28 23:27:18 +0300

    hw lwl1

--- hw/nikita_pakhomov/nlesson3/lesoon5hw.py
+++ hw/nikita_pakhomov/nlesson3/lesoon5hw.py
@@ -4,3 +4,8 @@ def strok(abc: str) -> str:
     strok2 = abc[prabel + 1 :]  # noqa
     strok3 = strok2 + abc[prabel] + strok1
     return strok3
+
+
+def lwl1(p1: tuple) -> tuple:
+    krt = (p1[0], p1[-1])
+    return krt
--- hw/nikita_pakhomov/test_hw5.py
+++ hw/nikita_pakhomov/test_hw5.py
@@ -1,3 +1,4 @@
+from hw.nikita_pakhomov.nlesson3.lesoon5hw import lwl1
 from hw.nikita_pakhomov.nlesson3.lesoon5hw import strok
 
 
@@ -7,3 +8,11 @@ def test_strok() -> None:
 
 if __name__ == "__main__":
     strok("111 222")
+
+
+def test_lwl1() -> None:
+    assert lwl1((1, 2, 3, 4)) == (1, 4)
+
+
+if __name__ == "__main__":
+    lwl1((1, 4))
