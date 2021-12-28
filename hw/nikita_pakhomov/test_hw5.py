e68f530988d73bce72efe0b7565a83a08fc43c1d
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-29 00:03:55 +0300

    hw lwl3

--- hw/nikita_pakhomov/test_hw5.py
+++ hw/nikita_pakhomov/test_hw5.py
@@ -1,4 +1,5 @@
 from hw.nikita_pakhomov.nlesson3.lesoon5hw import lwl1
+from hw.nikita_pakhomov.nlesson3.lesoon5hw import lwl3
 from hw.nikita_pakhomov.nlesson3.lesoon5hw import strok
 
 
@@ -16,3 +17,11 @@ def test_lwl1() -> None:
 
 if __name__ == "__main__":
     lwl1((1, 4))
+
+
+def test_lwl3() -> None:
+    assert lwl3([1, 2, 3, 4, 5], 2) == [1, 2]
+
+
+if __name__ == "__main__":
+    lwl3([1, 2, 3, 4, 5], 2)
