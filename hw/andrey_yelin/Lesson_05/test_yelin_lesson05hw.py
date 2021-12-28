7c2a19dbef08a195d5d0ac668847e6cf088fd79c
zyola <ded-undead@mail.ru>
2021-12-29 01:42:27 +0300

    First try

---
+++ hw/andrey_yelin/Lesson_05/test_yelin_lesson05hw.py
@@ -0,0 +1,21 @@
+from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import first_last_elements
+from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import slice_of_col03
+from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import string_combination
+from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import swapped_words
+from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import titled_string
+
+
+def test_example05() -> None:
+    col01 = [1, 2, 3, 4, 5, 6, 7]
+    col02 = "tuda suda"
+    some_object = 6
+    string01 = "j"
+    string02 = "FOR the glory of the Emperor"
+    assert first_last_elements(col01) == (1, 7)
+    assert swapped_words(col02) == "suda tuda"
+    assert slice_of_col03(col01, some_object) == [1, 2, 3, 4, 5, 6]
+    assert (
+        string_combination(string01, string02)
+        == "FjOjRj jtjhjej jgjljojrjyj jojfj jtjhjej jEjmjpjejrjojr"
+    )
+    assert titled_string(string02) == "For The Glory Of The Emperor"
---
+++ hw/andrey_yelin/Lesson_05/yelin_lesson05hw.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+col01 = [1, 2, 3, 4, 5, 6, 7]
+col02 = "tuda suda"
+
+
+def first_last_elements(collection: col01) -> tuple:
+    return (min(col01)), (max(col01))
+
+
+def swapped_words(collection: col02) -> str:
+    return f"{col02[-4:]} {col02[:4]}"
+
+
+def slice_of_col03(collection: Any, some_object: Any) -> list:
+    index = collection.index(some_object)
+    return collection[: index + 1]
+
+
+def string_combination(string01: str, string02: str) -> str:
+    return string01.join(string02)
+
+
+def titled_string(some_string: str) -> str:
+    return some_string.title()
