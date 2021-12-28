65e88590c85e1eb0327547ada49b9321d1c96a5f
tatsikuk <94784649+Tatsikuk26@users.noreply.github.com>
2021-12-28 10:00:42 +0300

    mod

--- hw/tatsiana_kukharskaya/test_lesson5.py
+++ hw/tatsiana_kukharskaya/test_lesson5.py
@@ -1,23 +1,21 @@
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it1
 
 
-def just_do_it1() -> None:
+def just_do_it() -> None:
     me = ["Help", "Me", "Please"]
     assert just_do_it1(me) == ("Help", "Please")
-
 
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it2
 
 
-def just_do_it2() -> None:
+def just_do_it() -> None:
     just_for_test = "true It's"
     assert just_do_it2(just_for_test) == "It's true"
-
 
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it3
 
 
-def just_do_it3() -> None:
+def just_do_it() -> None:
     nechto3 = ["It's", "so", "wonderfull", "for", "me"]
     nejki2 = nechto3[2]
     assert just_do_it3(nechto3, nejki2) == ["It's", "so", "wonderfull"]
@@ -26,7 +24,7 @@ def just_do_it3() -> None:
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it4
 
 
-def just_do_it4() -> None:
+def just_do_it() -> None:
     string_for_test = "Tatsin Vsses"
     char_for_test = "a"
     assert (
@@ -38,6 +36,6 @@ def just_do_it4() -> None:
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it5
 
 
-def just_do_it5() -> None:
+def just_do_it() -> None:
     for_test = "Do YoU like apple ?"  # noqa: E501
     assert just_do_it5(for_test) == "Do You Like Apple ?"  # noqa: E501,W503
