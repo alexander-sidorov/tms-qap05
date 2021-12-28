da370ad13210d14c697f86a2dceb62ccac1e1b0d
maryiasahanovich <m.s.saganovich@gmail.com>
2021-12-28 21:46:42 +0300

    lesson5 homework

--- hw/maria_saganovich/lesson5/task1.py
+++ hw/maria_saganovich/lesson5/task1.py
@@ -1,2 +1,2 @@
 def func1(list1: list) -> tuple:
-    return tuple([list1[0], list1[-1]])
+    return tuple([list1[0], list1[-1]])  # noqa: C409
--- hw/maria_saganovich/lesson5/task4.py
+++ hw/maria_saganovich/lesson5/task4.py
@@ -1,5 +1,5 @@
 def func4(some_string: str, some_string2: str) -> str:
-    result: str = ''
+    result: str = ""
     for char in some_string:
         result += char + some_string2
     return result
