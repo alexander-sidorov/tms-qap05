96c10debc2244954d48dd36e0cee14a03e110f86
Sasha <Pristigesasha@gmail.com>
2021-12-28 21:30:28 +0300

    Sasha Yaroshevich - Lesson 05

--- hw/sasha_yaroshevich/home_5_1.py
+++ hw/sasha_yaroshevich/home_5_1.py
@@ -1,17 +1,23 @@
-def funct(sort_collection):
+def funct(sort_collection) -> tuple:
     return (sort_collection[0], sort_collection[-1])
 
-def func(stroka_two_words):
-    a = stroka_two_words.split(',')
-    b = [a[-1], a[0]]
-    return ', '.join(b)
 
-def fun(sort_coll, p2):
-    return sort_coll[0:sort_coll.index(p2)]
+def func(stroka_two_words) -> str:
+    new_a = stroka_two_words.split(" ")
+    new_b = [new_a[-1], new_a[0]]
+    return " ".join(new_b)
 
-def fu(stroka, stroka_one):
-    new = list(stroka)
-    s = 'stroka_one'.join(new)
 
-def f(p1):
+def fun(sort_coll, p2) -> list:
+    limit = sort_coll.index(p2)
+    return sort_coll[0 : limit + 1]
 
+
+def fu(stroka, stroka_one) -> str:
+    new_stroka = list(stroka)
+    stroka_s = stroka_one.join(new_stroka)
+    return stroka_s
+
+
+def f(stroka_pr) -> str:  # noqa: E203
+    return stroka_pr.title()
