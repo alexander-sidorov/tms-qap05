7fe5a8c274f84b908a56a457c31f23feddd4b559
YaroslavBelaychuk <jaroslav.belaychuk@gmail.com>
2021-12-29 10:19:55 +0300

    bell23

--- hw/yaroslav_belaychuk/test_lesson06hw.py
+++ hw/yaroslav_belaychuk/test_lesson06hw.py
@@ -11,5 +11,5 @@ def test_function() -> None:
     assert (cortage([1, 2, 3, 8, 65, 99])) == (1, 99)
     assert (string("ping pong")) == "pong ping"
     assert (collection([20, 100, 30, 60], "cola")) == [20, 100, 30, 60, "cola"]
-    assert (str_in_str("abc", "**")) == "a**b**c"
+    assert (str_in_str("abc", "*")) == "a*b*c"
     assert (string_zaglavie("alTaVIsTA")) == "Altavista"
