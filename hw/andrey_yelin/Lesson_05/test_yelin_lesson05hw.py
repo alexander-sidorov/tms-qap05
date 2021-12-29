921b95aedd9976fdb0542e45fb8ac6715031dea2
zyola <ded-undead@mail.ru>
2021-12-29 19:04:14 +0300

    Make final changes

--- hw/andrey_yelin/Lesson_05/test_yelin_lesson05hw.py
+++ hw/andrey_yelin/Lesson_05/test_yelin_lesson05hw.py
@@ -6,14 +6,14 @@ from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import titled_string
 
 
 def test_example05() -> None:
-    col01 = [1+1j, 1-1j, 0]
+    col01 = [1 + 1j, 1 - 1j, 0]
     col02 = "bbbbbbbb                             aaaaaaaa"
-    some_object = 1-1j
+    some_object = 1 - 1j
     string01 = "j"
     string02 = "FOR the glory of the Emperor"
-    assert first_last_elements(col01) == (1+1j, 0)
+    assert first_last_elements(col01) == (1 + 1j, 0)
     assert swapped_words(col02) == "aaaaaaaa bbbbbbbb"
-    assert slice_of_col03(col01, some_object) == [1+1j, 1-1j]
+    assert slice_of_col03(col01, some_object) == [1 + 1j, 1 - 1j]
     assert (
         string_combination(string02, string01)
         == "FjOjRj jtjhjej jgjljojrjyj jojfj jtjhjej jEjmjpjejrjojr"  # noqa: W503, E501
