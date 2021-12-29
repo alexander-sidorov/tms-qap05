a20b87a3c41b808fa3a5e3e8a784d97fd63e5245
zyola <ded-undead@mail.ru>
2021-12-29 17:29:01 +0300

    Make some changes

--- hw/andrey_yelin/Lesson_05/test_yelin_lesson05hw.py
+++ hw/andrey_yelin/Lesson_05/test_yelin_lesson05hw.py
@@ -7,15 +7,15 @@ from hw.andrey_yelin.Lesson_05.yelin_lesson05hw import titled_string
 
 def test_example05() -> None:
     col01 = [1, 2, 3, 4, 5, 6, 7]
-    col02 = "tuda suda"
+    col02 = "bbbbbbbb                             aaaaaaaa"
     some_object = 6
     string01 = "j"
     string02 = "FOR the glory of the Emperor"
     assert first_last_elements(col01) == (1, 7)
-    assert swapped_words(col02) == "suda tuda"
+    assert swapped_words(col02) == "aaaaaaaa                             bbbbbbbb"
     assert slice_of_col03(col01, some_object) == [1, 2, 3, 4, 5, 6]
     assert (
         string_combination(string01, string02)
-        == "FjOjRj jtjhjej jgjljojrjyj jojfj jtjhjej jEjmjpjejrjojr"  # noqa: W503, E501
+        == "jFjOjRj jtjhjej jgjljojrjyj jojfj jtjhjej jEjmjpjejrjojr"  # noqa: W503, E501
     )
     assert titled_string(string02) == "For The Glory Of The Emperor"
--- hw/andrey_yelin/Lesson_05/yelin_lesson05hw.py
+++ hw/andrey_yelin/Lesson_05/yelin_lesson05hw.py
@@ -2,12 +2,12 @@ from typing import Any
 
 
 def first_last_elements(collection: Any) -> tuple:
-    return (min(collection)), (max(collection))
+    return collection[0], collection[-1]
 
 
 def swapped_words(string: str) -> str:
-    col02 = string
-    return f"{col02[-4:]} {col02[:4]}"
+    col02 = string.rsplit()
+    return f"{col02[1]} {col02[0]}"
 
 
 def slice_of_col03(collection: Any, some_object: Any) -> Any:
@@ -16,7 +16,7 @@ def slice_of_col03(collection: Any, some_object: Any) -> Any:
 
 
 def string_combination(string01: str, string02: str) -> str:
-    return string01.join(string02)
+    return string02.join(string01)
 
 
 def titled_string(some_string: str) -> str:
