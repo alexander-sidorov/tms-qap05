0fc41c04675b31b89d11f45f356eee43d6bb1430
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-30 02:24:18 +0300

    yyy

--- hw/nikita_pakhomov/nlesson3/lesoon5hw.py
+++ hw/nikita_pakhomov/nlesson3/lesoon5hw.py
@@ -19,8 +19,15 @@ def lwl3(
     p1: Sequence,
     p2: Any,
 ) -> Sequence:
-    cpisok = p1[:p2]
-    return cpisok
+    if type(p1) == str:
+        inde = p1.find(p2)
+        aza = p1[: inde + 1]
+    elif type(p1) == list:
+        aza = p1[:p2]
+    elif type(p1) == tuple:
+        inde = p1.index(p2)
+        aza = p1[: inde + 1]
+    return aza
 
 
 def lwl5(p1: str) -> str:
