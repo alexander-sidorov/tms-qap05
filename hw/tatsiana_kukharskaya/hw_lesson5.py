6861dcecac7cd575dc12368703c5055d562ec082
tatsikuk <94784649+Tatsikuk26@users.noreply.github.com>
2021-12-29 09:55:40 +0300

    modi

--- hw/tatsiana_kukharskaya/hw_lesson5.py
+++ hw/tatsiana_kukharskaya/hw_lesson5.py
@@ -1,10 +1,9 @@
-import operator
-
-
 def just_do_it1(me: list) -> tuple:
+    return (me[0], me[-1])
+
+
+def just_do_iit1(me: list) -> tuple:
     working = operator.itemgetter(0, -1)
-    f = working(me)
-    return tuple(f)
 
 
 def just_do_it2(wichtig: str) -> str:
@@ -13,16 +12,13 @@ def just_do_it2(wichtig: str) -> str:
 
 
 def just_do_it3(nejki: list, nejki2: str) -> list:
-
-    nechto1 = nechto(None, nejki.index(nejki2) + 1)
-    return nejki[nechto1]
+    nechto = nejki.index(nejki2)
+    return nejki[0 : nechto +1]
 
 
 def just_do_it4(string_1: str, string_2: str) -> str:
     string_res = ""
     for a in string_1:
-        string_res += a
-        string_res += string_2
     return string_res
 
 
--- hw/tatsiana_kukharskaya/test_lesson5.py
+++ hw/tatsiana_kukharskaya/test_lesson5.py
@@ -1,43 +1,25 @@
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it1
-
-
-def just_do_it() -> None:
-    me = ["Help", "Me", "Please"]
-    assert just_do_it1(me) == ("Help", "Please")
-
-
+from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_iit1
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it2
-
-
-def just_do_it() -> None:
-    just_for_test = "true It's"
-    assert just_do_it2(just_for_test) == "It's true"
-
-
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it3
-
-
-def just_do_it() -> None:
-    nechto3 = ["It's", "so", "wonderfull", "for", "me"]
-    nejki2 = nechto3[2]
-    assert just_do_it3(nechto3, nejki2) == ["It's", "so", "wonderfull"]
-
-
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it4
-
-
-def just_do_it() -> None:
-    string_for_test = "Tatsin Vsses"
-    char_for_test = "a"
-    assert (
-        just_do_it4(string_for_test, char_for_test)
-        == "Taatasaiana Vasasaeasa"  # noqa: W503
-    )
-
-
 from hw.tatsiana_kukharskaya.hw_lesson5 import just_do_it5
 
 
 def just_do_it() -> None:
+    me = ["Help", "Me", "Please"]
+    just_for_test = "true It's"
+    nechto3 = ["It's", "so", "wonderfull", "for", "me"]
+    nejki2 = nechto3[2]
+    string_for_test = "Tatsin Vsses"
+    char_for_test = "a"
     for_test = "Do YoU like apple ?"  # noqa: E501
+    assert just_do_it1(me) == ("Help", "Please")
+    assert just_do_iit1(me) == ("Help", "Please")
+    assert just_do_it2(just_for_test) == "It's true"
+    assert just_do_it3(nechto3, nejki2) == ["It's", "so", "wonderfull"]
+    assert (
+        just_do_it4(string_for_test, char_for_test)
+        == "Taatasaiana Vasasaeasa"  # noqa: W503
+    )
     assert just_do_it5(for_test) == "Do You Like Apple ?"  # noqa: E501,W503
