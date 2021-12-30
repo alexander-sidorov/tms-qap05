61931c6db4ec7da58bdb3c23c517f34131f96f8c
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2021-12-30 16:02:00 +0300

    xxxxx

--- hw/nikita_pakhomov/test_lesson5.py
+++ hw/nikita_pakhomov/test_lesson5.py
@@ -4,7 +4,3 @@ from hw.nikita_pakhomov.nlesson3.lesson5 import aggression
 def test_aggression() -> None:
     assert aggression(True) == "hhh"
     assert aggression(False) == "ggg"
-
-
-if __name__ == "__main__":
-    1
