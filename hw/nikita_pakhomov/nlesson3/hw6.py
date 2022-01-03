af1231009af35b69e6035cc86618ab6945497640
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-01-03 21:54:54 +0300

    hw6_1

--- hw/nikita_pakhomov/nlesson3/hw6.py
+++ hw/nikita_pakhomov/nlesson3/hw6.py
@@ -1,7 +1,7 @@
 def is_palindrome(text: str) -> dict:
     result = {}
     s1 = "".join(c for c in text if c.isalpha())
-    a = s1.lower()
-    reverst_a = a[::-1]
-    result["data"] = reverst_a == a
+    abc = s1.lower()
+    reverst_a = abc[::-1]
+    result["data"] = reverst_a == abc
     return result
