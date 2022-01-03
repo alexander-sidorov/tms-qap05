dc70ff59323a4b1b2574105e6874d8b98fa02d54
Sasha <Pristigesasha@gmail.com>
2022-01-03 21:23:17 +0300

    Sasha Yaroshevich - Lesson 06

--- hw/sasha_yaroshevich/test_home6.py
+++ hw/sasha_yaroshevich/test_home6.py
@@ -1,6 +1,6 @@
 from hw.sasha_yaroshevich.home_work6 import polindrom
 
 
-def test_home6_polindrom() -> None:
+def test_home6_polindrom() -> bool:
     polindrom_inp = "xyx"
     assert polindrom(polindrom_inp) == {"data": True}
