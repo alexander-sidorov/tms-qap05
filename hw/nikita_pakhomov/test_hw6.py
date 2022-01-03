77d225b42c33ccbf6eae19f1b89c0ff227752906
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-01-03 21:44:51 +0300

    hw6

--- hw/nikita_pakhomov/test_hw6.py
+++ hw/nikita_pakhomov/test_hw6.py
@@ -1,7 +1,7 @@
 from hw.nikita_pakhomov.nlesson3.hw6 import is_palindrome
 
 
-def test_is_palindrome():
+def test_is_palindrome() -> None:
     assert is_palindrome("") == {"data": True}
     assert is_palindrome("x") == {"data": True}
     assert is_palindrome("xx") == {"data": True}
