b2a889b523507bf54a96e1d7e50c5391ec08e166
siarhei_apanel <aposha_18@mail.ru>
2022-01-05 01:05:21 +0300

    HW3

--- hw/siarhei_apanel/refakt.py
+++ hw/siarhei_apanel/refakt.py
@@ -1,4 +1,5 @@
 from typing import Any
+from datetime import date
 
 
 def func(a1: float, back: float, cill: float) -> list:
@@ -60,144 +61,151 @@ def krypto(cod: str, key: str) -> str:
     return "".join(i1 if i1 == " " else alphavit[key.find(i1)] for i1 in cod)
 
 
-
-
-
-
-
-
-
-
-
-
-
-def palindrom(d1: str) -> dict:
+def palindrom(di1: str) -> dict:
     result = {}
-    if type(d1) != str:
+    if type(di1) != str:
         result["errors"] = ["TypeError"]
     else:
-        i = 0
-        j = len(d1) - 1
+        i34 = 0
+        j34 = len(di1) - 1
         is_palindrom = True
-        while i < j:
-            if d1[i] != d1[j]:
+        while i34 < j34:
+            if di1[i34] != di1[j34]:
                 is_palindrom = False
-            i += 1
-            j -= 1
-        result = {'data': True} if is_palindrom else {'data': False}
+            i34 += 1
+            j34 -= 1
+        result = {"data": True} if is_palindrom else {"data": False}
     return result
-
 
 
 def proizvedenie(*digit: tuple) -> dict:
     result = {}
     verif = 0
     product = 1
-            
+
     for dig in digit:
-               
+
         if type(dig) in [str, tuple, list]:
             verif += 1
             if verif >= 2:
-                result = {'errors': ["TypeError"]}
+                result = {"errors": ["TypeError"]}
                 break
-            
+
             else:
                 product *= dig
-                result = {'data': product}
+                result = {"data": product}
         else:
-                product *= dig
-                result = {'data': product}
-    
-        
+            product *= dig
+            result = {"data": product}
+
     return result
 
 
 def dateday(yer: int, mont: int, dayz: int) -> dict:
     result = {}
-    if type(yer) in [str, tuple, list] or type(mont) in [str, tuple, list] or type(dayz) in [str, tuple, list]: 
+    if (
+        type(yer) in [str, tuple, list]
+        or type(mont) in [str, tuple, list]
+        or type(dayz) in [str, tuple, list]
+    ):
         result["errors"] = ["TypeError"]
-    elif  yer == 0 or mont <= 0 or mont > 12 or dayz <= 0 or dayz > 31:
+    elif yer == 0 or mont <= 0 or mont > 12 or dayz <= 0 or dayz > 31:
         result["errors"] = ["ZeroError"]
     else:
-        from datetime import date
+
         data = date(yer, mont, dayz)
         now = date.today()
         delta = now - data
-        result = {'data':{"year": data.year, "month": data.month, "day": data.day, "age": int(delta.days//365)}}
+        result = {
+            "data": {
+                "year": data.year,
+                "month": data.month,
+                "day": data.day,
+                "age": int(delta.days // 365),
+            }
+        }
     return result
 
 
-from datetime import date
 def happybithday(yer: dict) -> dict:
-    age = date(1,1,1)
-    
+    age = date(1, 1, 1)
+
     for keys, value in yer.items():
-              
+
         if value > age:
             age = yer[keys]
-                                                   
-    return {'data': keys}
+
+    return {"data": keys}
 
 
-
-from typing import Any
 def repeat(collect: Any) -> dict:
-    
+
     noresult = []
     result = {}
-    
+
     for digit in collect:
         noresult.append(digit)
-    result_dict = {quant: noresult.count(quant) for quant in noresult if noresult.count(quant) > 1}
-    
+    result_dict = {
+        quant: noresult.count(quant)
+        for quant in noresult
+        if noresult.count(quant) > 1
+    }
+
     result["data"] = result_dict
     return result
 
 
 def html_str(query: str) -> dict:
     result = {}
-     
+
     for letter in query.split("&"):
         for el in range(len(letter.split("="))):
-            
+
             if letter.split("=")[el].isalpha() == True:
-                result.setdefault(letter.split("=")[el],[]).append(letter.split("=")[el + 1])
-                
+                result.setdefault(letter.split("=")[el], []).append(
+                    letter.split("=")[el + 1]
+                )
+
             else:
                 continue
-    return {'data': result}
+    return {"data": result}
 
 
 def decodding(code: str) -> dict:
-    return {"data":''.join(code[sym] * int(code[sym + 1]) for sym in range(len(code)) if code[sym].isalpha() == True)}
+    return {
+        "data": "".join(
+            code[sym] * int(code[sym + 1])
+            for sym in range(len(code))
+            if code[sym].isalpha() == True
+        )
+    }
 
 
 def codding(s1: str) -> dict:
     i1 = 0
-    x1 = 0
+    num = 0
     result = ""
     for i1 in range(len(s1)):
         j1 = i1 + 1
         if len(s1) == 1:
-            result += s1[0]+'1'
+            result += s1[0] + "1"
             break
         if j1 == len(s1):
-                t1 = (s1[i1])
-                if s1[-1] == s1[-2]:
-                    x1 += 1
-                    z1 = str(x1)
-                else:
-                    g1 = 1
-                    z1 = str(g1)
-                result += t1 + z1
-                break
-        x1 += 1
+            t1 = s1[i1]
+            if s1[-1] == s1[-2]:
+                num += 1
+                z1 = str(num)
+            else:
+                g1 = 1
+                z1 = str(g1)
+            result += t1 + z1
+            break
+        num += 1
         if s1[i1] != s1[j1]:
-            t1 = (s1[i1])
-            g1 = str(x1)
+            t1 = s1[i1]
+            g1 = str(num)
             result += t1 + g1
-            x1 = 0
+            num = 0
     return {"data": result}
 
 
@@ -208,67 +216,64 @@ def rever_dict(d1: dict) -> dict:
         spisok.append(value)
     for key, value in d1.items():
         if spisok.count(value) > 1:
-            result.setdefault(value,[]).append(key)
+            result.setdefault(value, []).append(key)
         else:
             result[value] = key
- 
+
     return {"data": result}
 
 
-from typing import Any
 def new_dict(key: Any, value: Any) -> dict:
     result = {}
     if len(key) > len(value):
-        len_none = len(key)-len(value)
+        len_none = len(key) - len(value)
         for i1 in range(len(value)):
             result[key[i1]] = value[i1]
-        for j1 in range(-len_none,0):
+        for j1 in range(-len_none, 0):
             result[key[j1]] = None
     elif len(key) == len(value):
         for _1 in range(len(value)):
-             result[key[_1]] = value[_1]
+            result[key[_1]] = value[_1]
     else:
-        len_none2 = len(value)-len(key)
+        len_none2 = len(value) - len(key)
         for z1 in range(len(key)):
             result[key[z1]] = value[z1]
-        for f1 in range(-len_none2,0):
-            result.setdefault(...,[]).append(value[f1])
-            
+        for f1 in range(-len_none2, 0):
+            result.setdefault(..., []).append(value[f1])
+
     return {"data": result}
 
 
 def new_set(set1: set, set2: set) -> dict:
-    return {"data": {'a&b': set1 & set2,'a|b': set1 | set2, 'a-b': set1 - set2, 'b-a': set2 - set1, '|a-b|': set1 ^ set2, 'a in b': set1 in set2, 'b in a': set2 in set1}}
+    return {
+        "data": {
+            "a&b": set1 & set2,
+            "a|b": set1 | set2,
+            "a-b": set1 - set2,
+            "b-a": set2 - set1,
+            "|a-b|": set1 ^ set2,
+            "a in b": set1 in set2,
+            "b in a": set2 in set1,
+        }
+    }
 
 
-from typing import Any
 def diction(*digit: Any) -> dict:
     if len(digit) % 2 != 0:
-        return  {'errors': ["NoPares"]}
-    for er in digit[ : : 2]:
+        return {"errors": ["NoPares"]}
+    for er in digit[::2]:
         if type(er) in [dict, set, list]:
-            return  {'errors': ["TypeError"]}
+            return {"errors": ["TypeError"]}
     result = {}
-    verif = 1      
+    verif = 1
     for dig in range(len(digit)):
-        
+
         if dig == verif:
             verif += 2
             continue
-        result[digit[dig]] = digit[dig+1]
-   
-    return  {"data": result}
+        result[digit[dig]] = digit[dig + 1]
 
-
-
-
-
-
-
-
-
-
-
+    return {"data": result}
 
 
 if __name__ == "__main__":
