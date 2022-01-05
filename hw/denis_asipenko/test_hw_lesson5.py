587da2c8c188bb2330ac3ba7be77961f67711aaa
siarhei_apanel <aposha_18@mail.ru>
2022-01-05 22:00:42 +0300

    HW6

--- hw/denis_asipenko/test_hw_lesson5.py
+++
@@ -1,6 +0,0 @@
-from hw.denis_asipenko.hw_lesson5 import aggression
-
-
-def test_homework5() -> None:
-    assert aggression(True) == "оно и видно"
-    assert aggression(False) == "а мог бы и знать"
--- hw/vlad_yancharski/test3.py
+++
@@ -1,6 +0,0 @@
-def f() -> None:
-    5  # noqa: B018
-
-
-def test_f() -> None:
-    assert f() is None  # type: ignore
