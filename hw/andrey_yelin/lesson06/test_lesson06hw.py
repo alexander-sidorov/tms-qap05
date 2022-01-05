b2b98993e97d1a7313705fc5db37f4dd41113efe
zyola <ded-undead@mail.ru>
2022-01-06 00:23:33 +0300

    First part of the HW6 v2

--- hw/andrey_yelin/lesson06/test_lesson06hw.py
+++ hw/andrey_yelin/lesson06/test_lesson06hw.py
@@ -1,19 +1,25 @@
+from datetime import date
+
 from hw.andrey_yelin.lesson06.functions_lesson06hw import ageResult
 from hw.andrey_yelin.lesson06.functions_lesson06hw import isPalindrome
 from hw.andrey_yelin.lesson06.functions_lesson06hw import multiplyArgs
 
-assert isPalindrome("") == {"data": True}
-assert isPalindrome("x") == {"data": True}
-assert isPalindrome("xx") == {"data": True}
-assert isPalindrome("xy") == {"data": False}
 
-assert multiplyArgs(1) == {"data": 1}
-assert multiplyArgs(1, 2) == {"data": 2}
-assert multiplyArgs(1, 2, 3) == {"data": 6}
+def test_isPalindrome() -> None:
+    assert isPalindrome("") == {"data": True}
+    assert isPalindrome("x") == {"data": True}
+    assert isPalindrome("xx") == {"data": True}
+    assert isPalindrome("xy") == {"data": False}
 
-from datetime import date
 
-d = date(year=1987, month=8, day=2)
-assert ageResult(d) == {
-    "data": {"year": 1987, "month": 8, "day": 2, "age": 34}
-}
+def test_multiplyArgs() -> None:
+    assert multiplyArgs(1) == {"data": 1}
+    assert multiplyArgs(1, 2) == {"data": 2}
+    assert multiplyArgs(1, 2, 3) == {"data": 6}
+
+
+def test_ageResult() -> None:
+    d = date(year=1987, month=8, day=2)
+    assert ageResult(d) == {
+        "data": {"year": 1987, "month": 8, "day": 2, "age": 34}
+    }
