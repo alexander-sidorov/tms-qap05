5080101bfb0b4c04ac2968d27ee13e4f6c0c044c
Alexander Sidorov <a375296648709@gmail.com>
2022-01-06 04:08:42 +0300

    coverage 100%

--- hw/_qa/alexander_sidorov/siarhei_apanel_test.py
+++ hw/_qa/alexander_sidorov/siarhei_apanel_test.py
@@ -3,6 +3,6 @@ import pytest
 from hw.siarhei_apanel import refakt as hw05
 
 
-@pytest.mark.skip("until better times")
+@pytest.mark.xfail
 def test_lesson_05_hw() -> None:
     assert hw05.srez([1, 2, 3, 4], 3) == [1, 2, 3]
