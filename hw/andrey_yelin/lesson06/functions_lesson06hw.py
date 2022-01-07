79cbb966414443931788c707e21cdf55d9f230b4
zyola <ded-undead@mail.ru>
2022-01-07 20:23:21 +0300

    First part of the HW6 v4

--- hw/andrey_yelin/lesson06/functions_lesson06hw.py
+++ hw/andrey_yelin/lesson06/functions_lesson06hw.py
@@ -1,9 +1,9 @@
 import functools
 from datetime import date
-from datetime import datetime
+from typing import Any
 
 
-def isPalindrome(s=None):
+def isPalindrome(s: Any = None) -> dict:  # noqa: N802, VNE001
     result = {}
     if s is None:
         result["errors"] = "none argument"
@@ -14,39 +14,39 @@ def isPalindrome(s=None):
     rev = "".join(reversed(s))
 
     if s == rev:
-        result["data"] = True
+        result["data"] = True  # type: ignore
     else:
-        result["data"] = False
+        result["data"] = False  # type: ignore
     return result
 
 
-def multiplyArgs(*m):
+def multiplyArgs(*m: Any) -> dict:  # noqa: N802
     result = {}
     args = [*m]
     if not len(args):
-        result["errors"] = "empty argument"
+        result["errors"] = "empty arguments"
         return result
     for i in args:
-        if not isinstance(i, int):
-            result["errors"] = "variable is not integer"
+        if not isinstance(i, int) and not isinstance(i, float):
+            result["errors"] = "variable is not a number"
             return result
     multiply = functools.reduce(lambda a, b: a * b, args)
     result["data"] = multiply
     return result
 
 
-def ageResult(born):
+def ageResult(born: Any) -> dict:  # noqa: N802
     result = {}
     if not isinstance(born, date):
-        result["errors"] = "variable is not date"
+        result["errors"] = "variable is not a date"
         return result
     today = date.today()
     age = int(
         today.year
-        - born.year
-        - ((today.month, today.day) < (born.month, born.day))
+        - born.year  # noqa: W503
+        - ((today.month, today.day) < (born.month, born.day))  # noqa: W503
     )
-    result["data"] = {
+    result["data"] = {  # type: ignore
         "year": born.year,
         "month": born.month,
         "day": born.day,
@@ -55,36 +55,32 @@ def ageResult(born):
     return result
 
 
-def older(oldDate):
+def older(oldDate: Any) -> dict:  # noqa: N803
     result = {}
     if len(oldDate) == 0:
         result["errors"] = "empty variable"
         return result
-    maxDate = 0
+    maxDate = 0  # noqa: N806
 
     for key in oldDate:
-        t = date.today()
+        t = date.today()  # noqa: VNE001
         ma = t - oldDate[key]
         dd = str(ma)
         if int(dd.split()[0]) > maxDate:
-            maxDate = int(dd.split()[0])
+            maxDate = int(dd.split()[0])  # noqa: N806
             result["data"] = key
     return result
 
 
-c = [(), "", "", 1, 1]
-
-
-def repeatingElements(elementsList):
+def repeatingElements(elementsList: Any) -> dict:  # noqa: N802, N803
     result = {}
-    zy = {}
+    zy = {}  # type: ignore
     repeat = {}
     for elem in elementsList:
         if elem in zy:
             zy[elem] = zy[elem] + 1
         else:
             zy[elem] = 1
-    print(zy)
     for key in zy:
         if zy[key] > 1:
             repeat[key] = zy[key]
@@ -92,5 +88,5 @@ def repeatingElements(elementsList):
         result["data"] = "elements are not repeat"
         return result
     else:
-        result["data"] = repeat
+        result["data"] = repeat  # type: ignore
     return result
--- hw/andrey_yelin/lesson06/test_lesson06hw.py
+++ hw/andrey_yelin/lesson06/test_lesson06hw.py
@@ -7,34 +7,41 @@ from hw.andrey_yelin.lesson06.functions_lesson06hw import older
 from hw.andrey_yelin.lesson06.functions_lesson06hw import repeatingElements
 
 
-def test_isPalindrome() -> None:
+def test_isPalindrome() -> None:  # noqa: N802
     assert isPalindrome("") == {"data": True}
     assert isPalindrome("x") == {"data": True}
     assert isPalindrome("xx") == {"data": True}
     assert isPalindrome("xy") == {"data": False}
+    assert isPalindrome(None) == {"errors": "none argument"}
+    assert isPalindrome(1) == {"errors": "not a string"}
 
 
-def test_multiplyArgs() -> None:
+def test_multiplyArgs() -> None:  # noqa: N802
     assert multiplyArgs(1) == {"data": 1}
     assert multiplyArgs(1, 2) == {"data": 2}
     assert multiplyArgs(1, 2, 3) == {"data": 6}
+    assert multiplyArgs() == {"errors": "empty arguments"}
+    assert multiplyArgs(1, "a") == {"errors": "variable is not a number"}
 
 
-def test_ageResult() -> None:
-    d = date(year=1987, month=8, day=2)
+def test_ageResult() -> None:  # noqa: N802
+    d = date(year=1987, month=8, day=2)  # noqa: VNE001
     assert ageResult(d) == {
         "data": {"year": 1987, "month": 8, "day": 2, "age": 34}
     }
+    assert ageResult(2) == {"errors": "variable is not a date"}
 
 
 def test_older() -> None:
-    b = {
+    b = {  # noqa: VNE001
         "A": date(year=2021, month=7, day=18),
         "B": date(year=1993, month=6, day=27),
     }
     assert older(b) == {"data": "B"}
+    assert older([]) == {"errors": "empty variable"}
+    assert older({}) == {"errors": "empty variable"}
 
 
-def test_repeatingElements() -> None:
-    c = [(), "", "", 1]
+def test_repeatingElements() -> None:  # noqa: N802
+    c = [(), "", "", 1]  # noqa: VNE001
     assert repeatingElements(c) == {"data": {"": 2}}
