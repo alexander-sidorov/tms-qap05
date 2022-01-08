8959937f0b8fd9d04d05968296ab08fc61e744cf
andrei9325 <andrei93.25@mail.ru>
2022-01-08 15:40:21 +0300

    5hw

---
+++ hw/andrei_bondar/bondar5hw.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+def fun1(collection: Any) -> tuple:
+    return collection[0], collection[-1]
+
+
+def fun2(string: str) -> str:
+    string1 = string.rsplit()
+    return f"{string1[1]} {string1[0]}"
+
+
+def fun3(collection1: Any, object: Any) -> Any:
+    collection2 = collection1.index(object)
+    return collection1[: collection2 + 1]
+
+
+def fun4(string1: str, string_v_1: str) -> str:
+    return string_v_1.join(string1)
+
+
+def fun5(string5: str) -> str:
+    return string5.title()
