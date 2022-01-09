4a29cb1a8f88f6d624ea1ae078b428a8562c8664
valeriyamavritskaya <valerymavritskaya@gmail.com>
2022-01-09 21:54:14 +0300

    Valeriya Mavritskaya - Lesson 06

--- hw/valeriya_mavritskaya/lesson06/homework06_functions.py
+++ hw/valeriya_mavritskaya/lesson06/homework06_functions.py
@@ -1,12 +1,11 @@
-from typing import Dict, Any
+import collections
+import re
 from datetime import date
 from math import prod
-import collections
-
-Result = Dict[str, Any]
+from typing import Any
 
 
-def if_palindrome_1(u_input: str) -> dict:
+def if_palindrome_1(u_input: Any) -> dict:
     result = {}
     if isinstance(u_input, str):
         u_input = u_input.lower()
@@ -23,7 +22,7 @@ def if_palindrome_1(u_input: str) -> dict:
     return result
 
 
-def multiplication_2(*args: int) -> dict:
+def multiplication_2(*args: Any) -> dict:
     result = {}
     if any(not isinstance(item, int) for item in args):
         result["error"] = "Input must be a number"
@@ -42,8 +41,8 @@ def birthday_3(input_date: date) -> dict:
             "year": input_date.year,
             "month": input_date.month,
             "day": input_date.day,
-            "age": age
-            }
+            "age": age,
+        }
     else:
         result["error"] = "Input must be a date"
     return result
@@ -56,51 +55,114 @@ def oldest_4(people: dict) -> dict:
     elif any(not isinstance(p, date) for p in people.values()):
         result["error"] = "Input must contain date"
     else:
-        result["data"] = f"{min(people, key=people.get)}"
+        result["data"] = f"{min(people, key=people.get)}"  # type: ignore
     return result
 
 
-def duplicates_5(collection: list) -> dict:
+def duplicates_5(collection: Any) -> dict:
+    if type(collection) not in [list, tuple, str]:
+        return {"error": "Invalid input"}
     result = {}
     select_duplicates = {i: collection.count(i) for i in collection}
-    duplicates_add_count = {key: value for (key, value) in select_duplicates.items() if value > 1}
-    result["data"] = duplicates_add_count
+    result["data"] = {
+        key: value for (key, value) in select_duplicates.items() if value > 1
+    }
     return result
+
+
+def dict_from_http_6(input_query: str) -> dict:
+    result: dict = {}
+    if isinstance(input_query, str):
+        pairs = input_query.split("&")
+        for pair in pairs:
+            parameter_and_value = pair.split("=")
+            parameter = parameter_and_value[0]
+            value = parameter_and_value[1]
+            if parameter in result:
+                result[parameter].append(value)
+            else:
+                result[parameter] = [value]
+    else:
+        result["error"] = "Input must be http query"
+    return {"data": result}
+
+
+def repeated_symbols_7(input_str: str) -> dict:
+    if isinstance(input_str, str):
+        groups = re.findall(r"(\D+\d+)", input_str)
+        result = ""
+        for element in groups:
+            symbol = re.findall(r"\D+", element)[0]
+            count = int(re.findall(r"\d+", element)[0])
+            result = result + symbol * count
+    else:
+        return {"error": "Invalid input"}
+    return {"data": result}
 
 
 def count_amount_8(str_input: str) -> dict:
-    result = {}
-    count = collections.Counter(str_input)
-    create_str = (f"{key}{value}" for (key, value) in count.items())
-    result["data"] = "".join(create_str)
+    if type(str_input) == str:
+        result = {}
+        count = collections.Counter(str_input)
+        create_str = (f"{key}{value}" for (key, value) in count.items())
+        result["data"] = "".join(create_str)
+    else:
+        return {"error": "Invalid input"}
     return result
 
 
-def mk_dictionary_12(*args) -> Result:
-    result: Result = {}
+def revert_dictionary_9(input_dictionary: dict) -> dict:
+    if type(input_dictionary) != dict:
+        return {"error": "Invalid input"}
+    result: dict[int, list] = {}
+    values_list = []
+    for value in input_dictionary.values():
+        values_list.append(value)
+    for key, value in input_dictionary.items():
+        if values_list.count(value) > 1:
+            result.setdefault(value, []).append(key)
+        else:
+            result[value] = key
+    return {"data": result}
+
+
+def join_dictionary_10(keys: Any, values: Any) -> dict:
+    dictionary_length = max(len(keys), len(values))
+    result: dict = {}
+    for index in range(dictionary_length):
+        should_use_source_key = len(keys) > index
+        should_use_source_value = len(values) > index
+        result_key = keys[index] if should_use_source_key else ...
+        result[result_key] = (
+            (values[index] if should_use_source_value else None)
+            if should_use_source_key
+            else [values[index]]
+            if ... not in result
+            else result[result_key] + [values[index]]
+        )
+    return {"data": result}
+
+
+def set_operations_11(set1: set, set2: set) -> dict:
+    if type(set1) != set or type(set2) != set:
+        return {"error": "Input should be set"}
+    result = {
+        "a&b": set1 & set2,
+        "a|b": set1 | set2,
+        "a-b": set1 - set2,
+        "b-a": set2 - set1,
+        "|a-b|": set1 ^ set2,
+        "a in b": set1.issubset(set2),
+        "b in a": set2.issubset(set1),
+    }
+    return {"data": result}
+
+
+def mk_dictionary_12(*args: Any) -> dict:
+    if len(args) % 2 != 0:
+        return {"error": "Use even number of elements"}
+    for elem in args:
+        if type(elem) == list:
+            return {"error": "Invalid type"}
     result = {args[i]: args[i + 1] for i in range(0, len(args), 2)}
-    return result
-
-
-if __name__ == '__main__':
-    print(if_palindrome_1("А муза рада музе без ума да разума"))
-    print(if_palindrome_1("bп"))
-    print(if_palindrome_1(True))
-
-    print(multiplication_2(1, 2, 3, 8))
-    print(multiplication_2((1, 2, 3, "dd")))
-
-    print(birthday_3(date(1993, 8, 3)))
-    print(birthday_3(1993))
-
-    print(oldest_4({"A": date(1993, 8, 3), "B": date(1993, 8, 3)}))
-    print(oldest_4({"A": date(year=2000, month=5, day=4), "B": date(year=1855, month=4, day=3)}))
-    print(oldest_4(1993))
-
-    print(duplicates_5([(), '', '', 1]))
-    print(duplicates_5([(), "", "", 1, 5, 7, 7, True, True]))
-    print(duplicates_5([]))
-
-    print(count_amount_8("aaabb"))
-
-    print(mk_dictionary_12(1, 8, 5, 5))
+    return {"data": result}
--- hw/valeriya_mavritskaya/lesson06/test_homework06.py
+++ hw/valeriya_mavritskaya/lesson06/test_homework06.py
@@ -1,32 +1,136 @@
-from hw.valeriya_mavritskaya.lesson06.homework06_functions import if_palindrome_1
-from hw.valeriya_mavritskaya.lesson06.homework06_functions import multiplication_2
-from hw.valeriya_mavritskaya.lesson06.homework06_functions import birthday_3
-from hw.valeriya_mavritskaya.lesson06.homework06_functions import oldest_4
 from datetime import date
+
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import birthday_3
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    count_amount_8,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    dict_from_http_6,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import duplicates_5
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    if_palindrome_1,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    join_dictionary_10,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    mk_dictionary_12,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    multiplication_2,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import oldest_4
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    repeated_symbols_7,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    revert_dictionary_9,
+)
+from hw.valeriya_mavritskaya.lesson06.homework06_functions import (
+    set_operations_11,
+)
 
 
 def test_if_palindrome_1() -> None:
-    assert if_palindrome_1("") == {'data': 'This is a palindrome'}
-    assert if_palindrome_1("x") == {'data': 'This is a palindrome'}
-    assert if_palindrome_1("xx") == {'data': 'This is a palindrome'}
-    assert if_palindrome_1("xy") == {'data': 'This is not a palindrome'}
-    assert if_palindrome_1(True) == {'error': 'Input must be a string'}
-    assert if_palindrome_1("А муза рада музе без ума да разума") == {'data': 'This is a palindrome'}
+    assert if_palindrome_1("") == {"data": "This is a palindrome"}
+    assert if_palindrome_1("x") == {"data": "This is a palindrome"}
+    assert if_palindrome_1("xx") == {"data": "This is a palindrome"}
+    assert if_palindrome_1("xy") == {"data": "This is not a palindrome"}
+    assert if_palindrome_1(True) == {"error": "Input must be a string"}
+    assert if_palindrome_1("А муза рада музе без ума да разума") == {
+        "data": "This is a palindrome"
+    }
 
 
 def test_multiplication_2() -> None:
-    assert multiplication_2(1) == {'data': 1}
-    assert multiplication_2(1, 2) == {'data': 2}
-    assert multiplication_2(1, 2, 3) == {'data': 6}
-    assert multiplication_2(1, 2, 3, 8) == {'data': 48}
-    assert multiplication_2(1, 2, 3, "dd") == {'error': 'Input must be a number'}
+    assert multiplication_2(1) == {"data": 1}
+    assert multiplication_2(1, 2) == {"data": 2}
+    assert multiplication_2(1, 2, 3) == {"data": 6}
+    assert multiplication_2(1, 2, 3, 8) == {"data": 48}
+    assert multiplication_2(1, 2, 3, "dd") == {
+        "error": "Input must be a number"
+    }
 
 
 def test_birthday_3() -> None:
-    assert birthday_3(date(year=1987, month=8, day=2)) == {'year': 1987, 'month': 8, 'day': 2, 'age': 34}
-    assert birthday_3(1987) == {'error': 'Input must be a date'}
+    assert birthday_3(date(year=1987, month=8, day=2)) == {
+        "year": 1987,
+        "month": 8,
+        "day": 2,
+        "age": 34,
+    }
+    assert birthday_3("1987") == {"error": "Input must be a date"}  # type: ignore  # noqa: E501
 
 
 def test_oldest_4() -> None:
-    assert oldest_4({"A": date(year=2000, month=5, day=4), "B": date(year=1855, month=4, day=3)}) == {'data': "B"}
-    assert oldest_4({"A": date(1993, 8, 3), "B": date(2000, 6, 6), "C": date(1980, 4, 5)}) == {'data': "C"}
+    assert oldest_4(
+        {
+            "A": date(year=2000, month=5, day=4),
+            "B": date(year=1855, month=4, day=3),
+        }
+    ) == {
+        "data": "B"
+    }  # noqa: W503
+    assert oldest_4(
+        {"A": date(1993, 8, 3), "B": date(2000, 6, 6), "C": date(1980, 4, 5)}
+    ) == {"data": "C"}
+
+
+def test_duplicates_5() -> None:
+    assert duplicates_5([(), "", "", 1]) == {"data": {"": 2}}
+
+
+def test_dict_from_http_6() -> None:
+    assert dict_from_http_6("x=1&x=2&y=3") == {
+        "data": {"x": ["1", "2"], "y": ["3"]}
+    }
+
+
+def test_repeated_symbols_7() -> None:
+    assert repeated_symbols_7("a3b2c1") == {"data": "aaabbc"}
+    assert repeated_symbols_7(False) == {"error": "Invalid input"}  # type: ignore  # noqa: E501
+
+
+def test_count_amount_8() -> None:
+    assert count_amount_8("aaabb") == {"data": "a3b2"}
+    assert count_amount_8(False) == {"error": "Invalid input"}  # type: ignore
+
+
+def test_revert_dictionary_9() -> None:
+    assert revert_dictionary_9({1: 100, 2: 100, 3: 300}) == {  # noqa: JS101
+        "data": {100: [1, 2], 300: 3}
+    }
+
+
+def test_join_dictionary_10() -> None:
+    assert join_dictionary_10("abc", [1, 2]) == {
+        "data": {"a": 1, "b": 2, "c": None}
+    }
+    assert join_dictionary_10("ab", [1, 2, 3]) == {
+        "data": {"a": 1, "b": 2, ...: [3]}
+    }
+
+
+def test_set_operations_11() -> None:
+    assert set_operations_11({1, 2}, {1, 3}) == {  # noqa: JS101
+        "data": {
+            "a&b": {1},
+            "a|b": {1, 2, 3},
+            "a-b": {2},
+            "b-a": {3},
+            "|a-b|": {2, 3},
+            "a in b": False,
+            "b in a": False,
+        }
+    }
+
+
+def test_mk_dictionary_12() -> None:
+    assert mk_dictionary_12(1, 2) == {"data": {1: 2}}
+    assert mk_dictionary_12(
+        1, True, (1, 2, 3), {3: 3}, "qwe", 7.8
+    ) == {  # noqa: JS101
+        "data": {1: True, (1, 2, 3): {3: 3}, "qwe": 7.8}
+    }
+    assert mk_dictionary_12(1, [1, 2, 34]) == {"error": "Invalid type"}
