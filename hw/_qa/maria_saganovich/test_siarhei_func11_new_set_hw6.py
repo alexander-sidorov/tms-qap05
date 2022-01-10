c3264db3c4aa4b2ff12dcad3f03d79aea80e2113
maryiasahanovich <m.s.saganovich@gmail.com>
2022-01-10 23:47:31 +0300

    corrections

--- hw/_qa/maria_saganovich/test_siarhei_func11_new_set_hw6.py
+++ hw/_qa/maria_saganovich/test_siarhei_func11_new_set_hw6.py
@@ -3,5 +3,7 @@ from hw.siarhei_apanel.refakt import new_set
 
 def test_func11_new_set() -> None:
     result = new_set({""}, [])
+    print(result)
     errors = result.get("errors")
+    assert type(errors) == list
     assert errors == sorted(errors)
--- hw/_qa/maria_saganovich/test_siarhei_repeat_hw6.py
+++ hw/_qa/maria_saganovich/test_siarhei_repeat_hw6.py
@@ -1,7 +1,7 @@
 from hw.siarhei_apanel.refakt import repeat
 
 
-def test_func5_repeat():
+def test_func5_repeat() -> None:
     c1: list = []
     result1 = repeat(c1)
     errors = result1.get("errors")
