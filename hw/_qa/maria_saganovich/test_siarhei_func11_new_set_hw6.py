066fa4e8b55e2a2a50fb791a8d77dfd153fbbfa1
maryiasahanovich <m.s.saganovich@gmail.com>
2022-01-11 00:02:27 +0300

    corrections

--- hw/_qa/maria_saganovich/test_siarhei_func11_new_set_hw6.py
+++ hw/_qa/maria_saganovich/test_siarhei_func11_new_set_hw6.py
@@ -4,5 +4,5 @@ from hw.siarhei_apanel.refakt import new_set
 def test_func11_new_set() -> None:
     result = new_set({""}, [])
     errors = result.get("errors")
-    assert type(errors) == list
+    assert isinstance(errors, list)
     assert errors == sorted(errors)
