08d4cdf4f7f6c8df6225a500edd437c4d1eca298
andrei9325 <andrei93.25@mail.ru>
2022-01-13 22:26:32 +0300

    6hw

---
+++ hw/andrei_bondar/lesson_06hw.py
@@ -0,0 +1,54 @@
+from typing import Any
+from datetime import date
+
+
+def palindrome(string: str) -> dict:
+    result = {}
+    if not isinstance(string, str):
+        return {"errors": "provided argument is not a string"}
+    else:
+        if string[:] == string[::-1]:
+            result["data"] = True
+        else:
+            result["data"] = False
+    return result
+
+
+def argum1(*args: Any) -> dict:
+    result = {}
+    try:
+        if len(args) < 0:
+            return {"errors": "no argument"}
+        elif len(args) == 1:
+            result["data"] = args[0]
+        else:
+            mresult = 1
+            for i in args:
+                mresult *= i
+                result["data"] = mresult
+    except TypeError:
+        return {"errors": "given arguments' types can't be multiplied"}
+    return result
+
+
+def datrog(d3: Any) -> dict:
+    result = {"errors": Any}
+    if type(d3) != date:
+        result["errors"] = ["be date"]
+    else:
+        s_age = {
+            "year": d3.year,
+            "month": d3.month,
+            "day": d3.day,
+            "age": int
+        }
+        today = date.today()
+        age = date.today().year - d3.year
+        if (d3.month > today.month) or (
+            d3.month == today.month and d3.day > today.day
+         ):
+            age = date.today().year - d3.year - 1
+        s_age["age"] = age
+        result = {"date": s_age}
+
+        return result
---
+++ hw/andrei_bondar/test_bondar6hw.py
@@ -0,0 +1,27 @@
+from datetime import date
+
+
+from hw.andrei_bondar.lesson_06hw import palindrome
+from hw.andrei_bondar.lesson_06hw import argum1
+from hw.andrei_bondar.lesson_06hw import datrog
+
+def test_palindrom() -> None:
+    assert palindrome("") == {"data": True}
+    assert palindrome(" ") == {"data": True}
+    assert palindrome("x") == {"data": True}
+    assert palindrome("xy") == {"data": False}
+    assert palindrome("xx") == {"data": True}
+
+
+def test_argum1() -> None:
+    assert argum1(1) == {"data": 1}
+    assert argum1(1, 2) == {"data": 2}
+    assert argum1(1, 2, 3) == {"data": 6}
+
+
+def test_datrog() -> None:
+    d1 = date(year=1987, month=8, day=2)
+    d2 = date(year=1911, month=1, day=1)
+
+    assert datrog(d1) == {"date": {"year": 1987, "month": 8, "day": 2, "age": 34}}
+    assert datrog(d2) == {"date": {"year": 1911, "month": 1, "day": 1, "age": 111}}
