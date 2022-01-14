979fa73059c4bbb0fe91b7effaec985e38f98a92
Андрей Елин <95867866+Zyola@users.noreply.github.com>
2022-01-14 23:38:22 +0300

    Andrey Yelin - Lesson 05hw (#117)
    * First try
    
    * First try
    
    * Make some changes
    
    * Make some changes
    
    * Make some changes
    
    * Make some changes
    
    * Make some changes
    
    * Make some changes
    
    * Make some changes
    
    * Make final changes
    
    * Make final changes
    
    * Last fix. Ready to merge!
    
    * Final

---
+++ hw/andrey_yelin/Lesson_05/yelin_lesson05hw.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+def first_last_elements(collection: Any) -> tuple:
+    return collection[0], collection[-1]
+
+
+def swapped_words(string: str) -> str:
+    col02 = string.rsplit()
+    return f"{col02[1]} {col02[0]}"
+
+
+def slice_of_col03(collection: Any, some_object: Any) -> Any:
+    index = collection.index(some_object)
+    return collection[: index + 1]
+
+
+def string_combination(string02: str, string01: str) -> str:
+    return string01.join(string02)
+
+
+def titled_string(some_string: str) -> str:
+    return some_string.title()
