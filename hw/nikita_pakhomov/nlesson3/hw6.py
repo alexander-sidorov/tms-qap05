36a0065d39956fc6a80c87ed3e142ba2aea86809
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-01-14 16:10:15 +0300

    hw6_lvl7

--- hw/nikita_pakhomov/nlesson3/hw6.py
+++ hw/nikita_pakhomov/nlesson3/hw6.py
@@ -105,13 +105,15 @@ def level_7(stroka: str) -> dict:
     if type(stroka) == str and stroka != "":
         if stroka[::2].isalpha() and stroka[1::2].isdecimal():
             if len(stroka[1::2]) == len(stroka[::2]):
+
                 for i in range(len(stroka)):
                     if stroka[i].isalpha():
                         aaa = aaa + stroka[i] * int(stroka[i + 1])
                         result = {"data": aaa}
+            else:
+                result["errors"] = "incorrect input"
 
         else:
-
             result["errors"] = "incorrect input"
     else:
 
