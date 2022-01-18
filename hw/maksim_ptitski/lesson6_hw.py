b51d9d2a2c1ce7de7085e0daf5cd7997a97d95bc
maksim_ptitski <marymaks@list.ru>
2022-01-18 22:09:38 +0300

    lesson-8-hw

--- hw/maksim_ptitski/lesson6_hw.py
+++ hw/maksim_ptitski/lesson6_hw.py
@@ -1,87 +1,267 @@
 import collections
+import re
+from collections import Counter
 from datetime import date
 from typing import Any
+from typing import Callable
 
 
-def is_the_string_is_the_palindrome(string: str) -> dict:
-    result = {}
+def decorator_dict(func: Callable) -> Callable:
+    def wrapper(*a: Any, **kw: Any) -> Any:
+        result = func(*a, **kw)
+        if isinstance(result, dict) and "errors" in result:
+            return result
+        else:
+            return {"data": result}
+
+    return wrapper
+
+
+@decorator_dict
+def is_the_string_is_the_palindrome(string: str) -> Any:
     if not isinstance(string, str):
-        return {"errors": "provided argument is not a string"}
+        return {"errors": ["given argument is not string type"]}
+    if string[:] == string[::-1]:
+        result = True
     else:
-        if string[:] == string[::-1]:
-            result["data"] = True
-        else:
-            result["data"] = False
+        result = False
     return result
 
 
-def data_multiplication(*any_data: Any) -> dict:
-    result = {}
+@decorator_dict
+def data_multiplication(*args: Any) -> Any:
+    result = 0
     try:
-        if len(any_data) < 1:
-            return {"errors": "must be more than 0 arguments"}
-        elif len(any_data) == 1:
-            result["data"] = any_data[0]
+        if len(args) < 1:
+            return {"errors": ["no argument is given"]}
+        elif len(args) == 1:
+            result = args[0]
         else:
-            mult_result = 1
-            for i in any_data:
-                mult_result *= i
-                result["data"] = mult_result
+            multiplication_result = 1
+            for i in args:
+                multiplication_result *= i
+                result = multiplication_result
     except TypeError:
-        return {"errors": "given arguments' types can't be multiplied"}
+        return {"errors": ["given arguments' types can't be multiplied"]}
     return result
 
 
-def how_old_are_you(birthday: date) -> dict:
-    result = {}
+@decorator_dict
+def how_old_are_you(birthday_date: date) -> dict:
+    if not isinstance(birthday_date, date):
+        return {"errors": ["given argument is not object of date type"]}
     today = date.today()
-    if not isinstance(birthday, date):
-        return {"errors": "wrong type of provided data"}
-    if today.year < birthday.year:
-        return {"errors": "you're not born yet"}
+    birthday_this_year = date(
+        today.year, birthday_date.month, birthday_date.day
+    )
+    if birthday_this_year > today:
+        age = today.year - birthday_date.year - 1
     else:
-        result_age = today.year - birthday.year - 1
-        if (
-            birthday.month == today.month
-            and birthday.day <= today.day  # noqa: W503
-            or birthday.month < today.month  # noqa: W503
-        ):
-            result_age += 1
-
-        result["data"] = {
-            "year": birthday.year,
-            "month": birthday.month,
-            "day": birthday.day,
-            "age": result_age,
-        }
-        return result
+        age = today.year - birthday_date.year
+    result = {
+        "year": birthday_date.year,
+        "month": birthday_date.month,
+        "day": birthday_date.day,
+        "age": age,
+    }
+    return result
 
 
-def the_oldest_one(birthday: dict) -> dict:
-    if not isinstance(birthday, dict):
-        return {"errors": "Given argument is not a dictionary"}
-    else:
-        name_result = ""
-        temp = []
-        for value in birthday.values():
-            temp.append(value)
-        min_t = min(temp)
-        for keys, value in birthday.items():
-            if value == min_t:
-                name_result = keys
-        return {"data": name_result}
+@decorator_dict
+def the_oldest_one(dictionary: dict[Any, date]) -> Any:
+    if not isinstance(dictionary, dict):
+        return {"errors": ["Given argument is not a dictionary"]}
+    for _key, value in dictionary.items():
+        if not isinstance(value, date):
+            return {"errors": ["key value is not object of date type"]}
+    min_value = min(dictionary, key=lambda k: dictionary[k])
+    result = min_value
+    return result
 
 
+@decorator_dict
 def repeated_symbols(collection: Any) -> dict:
-    types = (list, str, tuple)
+    types = (list, str, tuple, set, dict)
     if type(collection) not in types:
-        return {"errors": "given arguments are not a list, str or tuple"}
+        return {
+            "errors": ["given argument with keys is not a list, str or tuple"]
+        }
+    try:
+        counter: Counter = collections.Counter(collection)
+    except TypeError:
+        return {"errors": ["collection contains unhashable type"]}
+    result_dict = {}
+    for key, value in counter.items():
+        if value == 1:
+            continue
+        else:
+            result_dict[key] = value
+    result = result_dict
+    return result
+
+
+@decorator_dict
+def http_query(query: str) -> dict:
+    if not isinstance(query, str):
+        return {"errors": ["given argument is not string type"]}
+    dict_with_queries = {}
+    try:
+        split_query = query.split("&")
+        for i in split_query:
+            index_of_equals = i.index("=")
+            name = i[:index_of_equals]
+            if name not in dict_with_queries:
+                dict_with_queries[name] = [
+                    i[index_of_equals + 1 :]  # noqa: E203
+                ]
+            else:
+                dict_with_queries[name].append(
+                    i[index_of_equals + 1 :]  # noqa: E203
+                )  # noqa: E203
+    except IndexError and ValueError:
+        return {"errors": ["given query contains wrong format"]}
+    result = dict_with_queries
+    return result
+
+
+@decorator_dict
+def repeat_chars(string: str) -> Any:
+    if not isinstance(string, str):
+        return {"errors": ["given argument is not string type"]}
+    numbers = re.findall(r"\d+", string)
+    chars = re.findall(r"\D", string)
+    if len(numbers) != len(chars):
+        return {"errors": ["wrong format of string"]}
     else:
-        dict_result: dict = collections.Counter(collection)
-        result = {}
-        result_dict = {}
-        for key, value in dict_result.items():
-            if value != 1:
-                result_dict[key] = value
-        result["data"] = result_dict
+        zipped_lists = list(zip(numbers, chars))
+        result_str = ""
+        for num, char in zipped_lists:
+            result_str += char * int(num)
+        result = result_str
+    return result
+
+
+@decorator_dict
+def count_chars(string: str) -> Any:
+    if not isinstance(string, str):
+        return {"errors": ["given argument is not string type"]}
+    if len(string) == 0:
+        result = ""
         return result
+    if len(string) == 1:
+        result = f"{string[0]}1"
+        return result
+    result_str = ""
+    counter: dict = {string[0]: 0}
+    previous_value = string[0]
+    for letter in string:
+        if letter in counter:
+            counter[letter] += 1
+        else:
+            counter[letter] = 1
+            substring = f"{previous_value}{counter[previous_value]}"
+            result_str += substring
+            counter.pop(previous_value)
+            previous_value = letter
+    result_str += f"{list(counter.keys())[0]}{list(counter.values())[0]}"
+    result = result_str
+    return result
+
+
+@decorator_dict
+def inverted_dictionary(dictionary: dict) -> dict:
+    if not isinstance(dictionary, dict):
+        return {"errors": ["given argument is not dict type"]}
+    inverted_dict = {}
+    try:
+        for key, value in dictionary.items():
+            if value not in inverted_dict:
+                inverted_dict[value] = [key]
+            else:
+                inverted_dict[value].append(key)
+    except TypeError:
+        return {"errors": ["dictionary value contains unhashable type"]}
+    for new_key, new_value in inverted_dict.items():
+        if len(new_value) < 2:
+            inverted_dict[new_key] = new_value[0]
+    result = inverted_dict
+    return result
+
+
+@decorator_dict
+def zip_collections_to_dict(keys: Any, values: Any) -> dict:
+    result: dict = {}
+    errors: list = []
+    types = (list, str, tuple)
+    unhashable_types = (list, set, dict)
+    error_txt1 = "given argument with keys is not a list, str or tuple"
+    error_txt2 = "given argument with values is not a list, str or tuple"
+    error_txt3 = ["collections contain values with unhashable type"]
+    if type(keys) not in types:
+        errors.append(error_txt1)
+    if type(values) not in types:
+        errors.append(error_txt2)
+    if errors:
+        result["errors"] = errors
+    else:
+        if any((isinstance(keys, unhashable_types) for x in keys)):
+            return {"errors": error_txt3}
+        keys = list(keys)
+        values = list(values)
+        if len(keys) > len(values):
+            differance = len(keys) - len(values)
+            values.extend(None for i in range(differance))
+            list_of_zipped_collections = list(zip(keys, values))
+            result = dict(list_of_zipped_collections)
+        elif len(keys) < len(values):
+            differance_list = values[len(keys) :]  # noqa: E203
+            list_with_the_same_length = values[: len(keys)]
+            list_of_zipped_collections = list(
+                zip(keys, list_with_the_same_length)
+            )
+            list_of_zipped_collections.append(("...", differance_list))
+            result = dict(list_of_zipped_collections)
+        else:
+            list_of_zipped_collections = list(zip(keys, values))
+            result = dict(list_of_zipped_collections)
+    return result
+
+
+@decorator_dict
+def relations_between_two_sets(set1: set, set2: set) -> dict:
+    errors: list = []
+    if not isinstance(set1, set):
+        errors.append("given first argument is not set type")
+    if not isinstance(set2, set):
+        errors.append("given second argument is not set type")
+    if errors:
+        return {"errors": errors}
+    return {
+        "a&b": set1 & set2,
+        "a|b": set1 | set2,
+        "a-b": set1 - set2,
+        "b-a": set2 - set1,
+        "|a-b|": set1 ^ set2,
+        "a in b": set1.issubset(set2),
+        "b in a": set2.issubset(set1),
+    }
+
+
+@decorator_dict
+def make_dictionary(*args: Any) -> dict:
+    if len(args) % 2 != 0:
+        return {"errors": ["Quantity of given arguments is not even"]}
+    else:
+        keys = []
+        values = []
+        for i in args:
+            if args.index(i) % 2 == 0:
+                keys.append(i)
+            else:
+                values.append(i)
+        try:
+            dictionary_from_arguments = dict(list(zip(keys, values)))
+        except TypeError:
+            return {"errors": ["odd argument is unhashable type"]}
+        result = dictionary_from_arguments
+    return result
--- hw/maksim_ptitski/test_lesson6_hw.py
+++ hw/maksim_ptitski/test_lesson6_hw.py
@@ -1,10 +1,17 @@
 from datetime import date
 
+from hw.maksim_ptitski.lesson6_hw import count_chars
 from hw.maksim_ptitski.lesson6_hw import data_multiplication
 from hw.maksim_ptitski.lesson6_hw import how_old_are_you
+from hw.maksim_ptitski.lesson6_hw import http_query
+from hw.maksim_ptitski.lesson6_hw import inverted_dictionary
 from hw.maksim_ptitski.lesson6_hw import is_the_string_is_the_palindrome
+from hw.maksim_ptitski.lesson6_hw import make_dictionary
+from hw.maksim_ptitski.lesson6_hw import relations_between_two_sets
+from hw.maksim_ptitski.lesson6_hw import repeat_chars
 from hw.maksim_ptitski.lesson6_hw import repeated_symbols
 from hw.maksim_ptitski.lesson6_hw import the_oldest_one
+from hw.maksim_ptitski.lesson6_hw import zip_collections_to_dict
 
 
 def test_is_the_string_is_the_palindrome() -> None:
@@ -14,7 +21,7 @@ def test_is_the_string_is_the_palindrome() -> None:
     assert is_the_string_is_the_palindrome("xx") == {"data": True}
     assert is_the_string_is_the_palindrome("abccba") == {"data": True}
     assert is_the_string_is_the_palindrome("xyz") == {"data": False}
-    assert is_the_string_is_the_palindrome(123321) == {  # type: ignore
+    assert is_the_string_is_the_palindrome(123321) == {
         "errors": "provided argument is not a string"
     }
 
@@ -45,9 +52,7 @@ def test_how_old_are_you() -> None:
     assert how_old_are_you(d2) == {
         "data": {"year": 1987, "month": 1, "day": 1, "age": 35}
     }
-    assert how_old_are_you(198782) == {  # type: ignore
-        "errors": "wrong type of provided data"
-    }
+    assert how_old_are_you(198782) == {"errors": "wrong type of provided data"}
     assert how_old_are_you(d3) == {"errors": "you're not born yet"}
 
 
@@ -62,7 +67,7 @@ def test_the_oldest_one() -> None:
     }
     assert the_oldest_one(d1) == {"data": "B"}
     assert the_oldest_one(d2) == {"data": "A"}
-    assert the_oldest_one(151654684) == {  # type: ignore
+    assert the_oldest_one(151654684) == {
         "errors": "Given argument is not a dictionary"
     }
 
@@ -78,3 +83,135 @@ def test_repeated_symbols() -> None:
     assert repeated_symbols(9099323) == {
         "errors": "given arguments are not a list, str or tuple"
     }
+
+
+def test_repeat_chars() -> None:
+    assert repeat_chars("a3b2c1") == {"data": "aaabbc"}
+    assert repeat_chars(["a3b2c1"]) == {
+        "errors": ["given argument is not string type"]
+    }
+    assert repeat_chars("a3b2c") == {"errors": ["wrong format of string"]}
+    assert repeat_chars("13b2c1") == {"errors": ["wrong format of string"]}
+    assert repeat_chars("a3b2cv") == {"errors": ["wrong format of string"]}
+    assert repeat_chars("a11") == {"data": "aaaaaaaaaaa"}
+    assert repeat_chars("a1b1a1") == {"data": "aba"}
+
+
+def test_count_chars() -> None:
+    assert count_chars("aaabb") == {"data": "a3b2"}
+    assert count_chars("aaabb111!!") == {"data": "a3b213!2"}
+    assert count_chars(12345) == {
+        "errors": ["given argument is not string type"]
+    }
+    assert count_chars("") == {"data": ""}
+    assert count_chars("a") == {"data": "a1"}
+    assert count_chars("aba") == {"data": "a1b1a1"}
+
+
+def test_inverted_dictionary() -> None:
+    d1 = {1: 100, 2: 100, 3: 300}
+    d2 = {1: {1: 100, 2: 200}, 2: 100, 3: 300}
+    assert inverted_dictionary(d1) == {"data": {100: [1, 2], 300: 3}}
+    assert inverted_dictionary(1234) == {
+        "errors": ["given argument is not dict type"]
+    }
+    assert inverted_dictionary(d2) == {
+        "errors": ["dictionary value contains unhashable type"]
+    }
+    assert inverted_dictionary({}) == {"data": {}}
+
+
+def test_zip_collections_to_dict() -> None:
+    assert zip_collections_to_dict("abc", [1, 2]) == {
+        "data": {"a": 1, "b": 2, "c": None}
+    }
+    assert zip_collections_to_dict("ab", [1, 2, 3]) == {
+        "data": {"a": 1, "b": 2, "...": [3]}
+    }
+    assert zip_collections_to_dict("abc", [5, 6, 7]) == {
+        "data": {"a": 5, "b": 6, "c": 7}
+    }
+    assert zip_collections_to_dict("abc", 123) == {
+        "errors": ["given argument with values is not a list, str or tuple"]
+    }
+    assert zip_collections_to_dict(123, "abc") == {
+        "errors": ["given argument with keys is not a list, str or tuple"]
+    }
+    assert zip_collections_to_dict(123, 456) == {
+        "errors": [
+            "given argument with keys is not a list, str or tuple",
+            "given argument with values is not a list, str or tuple",
+        ]
+    }
+    assert zip_collections_to_dict(
+        [{1: 2}, 4, 5], [1, 2, 3]
+    ) == {  # noqa: JS101
+        "errors": ["collections contain values with unhashable type"]
+    }
+    assert zip_collections_to_dict([{1: 2}, 4], [1, 2, 3],) == {  # noqa: JS101
+        "errors": ["collections contain values with unhashable type"]
+    }  # noqa: W503
+    assert zip_collections_to_dict([{1: 2}, 3, 4], [1, 2],) == {  # noqa: JS101
+        "errors": ["collections contain values with unhashable type"]
+    }  # noqa: W503
+
+
+def test_relations_between_two_sets() -> None:
+    error1 = {"errors": ["given first argument is not set type"]}
+    error2 = {"errors": ["given second argument is not set type"]}
+    error3 = {
+        "errors": [
+            "given first argument is not set type",
+            "given second argument is not set type",
+        ]
+    }
+    assert relations_between_two_sets({1, 2}, {1, 3}) == {  # noqa: JS101
+        "data": {
+            "a&b": {1},
+            "a|b": {1, 2, 3},
+            "a-b": {2},
+            "b-a": {3},
+            "|a-b|": {2, 3},
+            "a in b": False,
+            "b in a": False,
+        }
+    }
+    assert relations_between_two_sets({1, 2}, {1, 2, 3}) == {  # noqa: JS101
+        "data": {
+            "a&b": {1, 2},
+            "a|b": {1, 2, 3},
+            "a-b": set(),
+            "b-a": {3},
+            "|a-b|": {3},
+            "a in b": True,
+            "b in a": False,
+        }
+    }
+    assert relations_between_two_sets(123, {1, 2, 3}) == error1
+    assert relations_between_two_sets({1, 2, 3}, 123) == error2
+    assert relations_between_two_sets(123, 123) == error3
+
+
+def test_make_dictionary() -> None:
+    assert make_dictionary(1, 2) == {"data": {1: 2}}
+    assert make_dictionary(1, 2, 3) == {
+        "errors": ["Quantity of given arguments is not even"]
+    }
+    assert make_dictionary({1: 2, 2: 3}, 5) == {  # noqa: JS101
+        "errors": ["odd argument is unhashable type"]
+    }
+
+
+def test_http_query() -> None:
+    assert http_query("x=1&x=2&y=3") == {"data": {"x": ["1", "2"], "y": ["3"]}}
+    assert http_query("xyz=123&xyz=abc&qqq=348") == {
+        "data": {"xyz": ["123", "abc"], "qqq": ["348"]}
+    }
+    assert http_query("x=&x=2&q=3") == {"data": {"x": ["", "2"], "q": ["3"]}}
+    assert http_query(["x=1&x=2&y=3"]) == {
+        "errors": ["given argument is not string type"]
+    }
+    assert http_query("x=1&&x=2&q=3") == {
+        "errors": ["given query contains wrong format"]
+    }
+    assert http_query("xxx=&yyy=") == {"data": {"xxx": [""], "yyy": [""]}}
