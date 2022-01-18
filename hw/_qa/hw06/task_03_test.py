6a2cccc32007774125965316801b1d4ad63e30a8
Alexander Sidorov <76160017+alexander-sidorov@users.noreply.github.com>
2022-01-18 18:28:30 +0300

    Homework 06 - Full tests (#141)
    * added tests for framework
    
    * fix my code
    
    * added more tests

---
+++ hw/_qa/hw06/task_03_test.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+import pytest
+
+from hw._qa.hw06.common import validate_data
+from hw._qa.hw06.common import validate_errors
+from hw.alexander_sidorov.lesson06.task03 import task_03 as alexander_sidorov
+
+from .common import azaza
+from .common import qual_name
+from .datasets.task_03 import happy_data
+from .datasets.task_03 import unhappy_data
+
+solutions = [
+    pytest.param(solution, id=qual_name(solution))
+    for solution in {
+        alexander_sidorov,
+    }
+]
+
+
+@pytest.mark.parametrize("arg,expected", happy_data)
+@pytest.mark.parametrize("solution", solutions)
+def test_task_03_happy(solution: Any, arg: Any, expected: Any) -> None:
+    result = solution(arg)
+    validate_data(result)
+
+    data = result["data"]
+    assert isinstance(data, dict)
+
+    keys = {"year", "month", "day", "age"}
+    aza = azaza()
+    for key in keys:
+        assert (value := data.get(key, aza)) is not aza
+        assert isinstance(value, int)
+
+    age = data["age"]
+    assert age == expected
+
+
+@pytest.mark.parametrize("arg", unhappy_data)
+@pytest.mark.parametrize("solution", solutions)
+def test_task_03_unhappy(solution: Any, arg: Any) -> None:
+    result = solution(arg)
+    validate_errors(result)
