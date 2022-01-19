745f74fd074c3f9e88d73b04b3ff6018ff52e164
andrei9325 <andrei93.25@mail.ru>
2022-01-19 15:36:10 +0300

    6hw

--- hw/andrei_bondar/lesson_06hw.py
+++ hw/andrei_bondar/lesson_06hw.py
@@ -2,35 +2,48 @@ from datetime import date
 from typing import Any
 
 
-def palindrome(string: str) -> dict:
+def decorat(func: Any) -> Any:
+    def wrapper(*args1: Any, **kw: Any) -> Any:
+        result = func(*args1, **kw)
+        if isinstance(result, dict) and "errors" in result:
+            return result
+        return {"data": result}
+
+    return wrapper
+
+
+@decorat
+def palindrome(string: str) -> Any:
     result = {}
     if not isinstance(string, str):
         return {"errors": "provided argument is not a string"}
     else:
         if string[:] == string[::-1]:
-            result["data"] = True
+            result = True
         else:
-            result["data"] = False
+            result = False
     return result
 
 
-def argum1(*args: Any) -> dict:
-    result = {}
+@decorat
+def argum1(*args: Any) -> Any:
+    result = 0
     try:
-        if len(args) < 0:
-            return {"errors": "no argument"}
+        if len(args) < 1:
+            return {"errors": ["no argument"]}
         elif len(args) == 1:
-            result["data"] = args[0]
+            result = args[0]
         else:
             mresult = 1
             for i in args:
                 mresult *= i
-                result["data"] = mresult
+                result = mresult
     except TypeError:
-        return {"errors": "given arguments' types can't be multiplied"}
+        return {"errors": ["given arguments' types can't be multiplied"]}
     return result
 
 
+@decorat
 def datrog(d3: Any) -> dict:
     result = {"errors": Any}
     if type(d3) != date:
@@ -50,9 +63,10 @@ def datrog(d3: Any) -> dict:
     return result
 
 
-def ber4(birthday: dict[Any, date]) -> dict:
+@decorat
+def ber4(birthday: dict[Any, date]) -> Any:
     if not isinstance(birthday, dict):  # определяет тип аргумента
-        return {"errors": Any}
+        return {"errors": ["not a dictionary"]}
     else:
         name_result = min(birthday, key=lambda t: birthday[t])
         return {"data": name_result}
