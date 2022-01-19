92a435b091c50719c5548f4b86561dac85f2cac7
Alexander Sidorov <a375296648709@gmail.com>
2022-01-19 03:30:02 +0300

    regression (2)

--- hw/_qa/hw06/task_05_test.py
+++ hw/_qa/hw06/task_05_test.py
@@ -5,6 +5,7 @@ import pytest
 from hw._qa.hw06.common import validate_data
 from hw._qa.hw06.common import validate_errors
 from hw.alexander_sidorov.lesson06.task05 import task_05 as alexander_sidorov
+from hw.siarhei_apanel.refakt import repeat as siarhei_apanel
 
 from .common import qual_name
 from .datasets.task_05 import happy_data
@@ -14,6 +15,7 @@ solutions = [
     pytest.param(solution, id=qual_name(solution))
     for solution in {
         alexander_sidorov,
+        siarhei_apanel,
     }
 ]
 
