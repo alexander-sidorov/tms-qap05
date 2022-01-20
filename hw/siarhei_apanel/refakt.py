ea93aa222d93f8c6ac8ad6c6c5d46c40c4b35b85
siarhei_apanel <aposha_18@mail.ru>
2022-01-20 05:43:29 +0300

    Decorator_Lambda

--- hw/siarhei_apanel/refakt.py
+++ hw/siarhei_apanel/refakt.py
@@ -71,7 +71,7 @@ def decor_data(func: Callable) -> Callable:
         try:
             return {"data": func(*args, **kwargs)}
         except Exception as f:
-            return {"errors": f}
+            return {"errors": str(f)}
 
     return wrapper
 
