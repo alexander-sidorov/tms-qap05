ec3c912e46236ac995e1a5186b3a8b922e821315
Alexander Sidorov <a375296648709@gmail.com>
2022-01-20 17:20:27 +0300

    increased coverage

--- hw/alexander_sidorov/lesson06/common_test.py
+++ hw/alexander_sidorov/lesson06/common_test.py
@@ -1,6 +1,12 @@
 from typing import Callable
+from typing import Sequence
+from typing import TypeVar
+from typing import Union
 
+from .common import name
 from .common import validate_args_types
+
+T = TypeVar("T")
 
 
 @validate_args_types
@@ -10,3 +16,11 @@ def func(arg: Callable) -> None:
 
 def test_validator() -> None:
     func(func)
+
+
+def test_name() -> None:
+    assert name(Union) == "Union"
+    assert name(Sequence) == "Sequence"
+    assert name(list) == "list"
+    assert name([]) == "[]"
+    assert name(T) == "T"
