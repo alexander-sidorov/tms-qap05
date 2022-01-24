c0dc192254730d70a7f6c55a91557ad22155c44d
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-01-24 23:03:58 +0300

    hw

--- hw/nikita_pakhomov/nlesson3/hw10.py
+++ hw/nikita_pakhomov/nlesson3/hw10.py
@@ -1,11 +1,11 @@
 from typing import Any
 
-from hw.nikita_pakhomov.nlesson3.hw6 import palindrome
+from hw.nikita_pakhomov.nlesson3.hw6 import is_palindrome
 
 
 class Palindrome01:
     def __init__(self, stroka: Any) -> None:
         self.stroka = stroka
 
-    def __bool__(self) -> Any:
-        return palindrome(self.stroka)
+    def __ccc__(self) -> Any:
+        return is_palindrome(self.stroka)
