b8b435acba6d9aaace2dd96ad208f4ff82dd0811
Brlinetta <81052038+Brlinetta@users.noreply.github.com>
2022-01-25 22:19:44 +0300

    hw 10

--- hw/nikita_pakhomov/test_hw10.py
+++ hw/nikita_pakhomov/test_hw10.py
@@ -9,6 +9,6 @@ def test_is_palindrome01() -> None:
     assert not Palindrome01("xy x")
 
 
-def test_is_User02() -> None:
-    d = date(2020, 1, 23)
-    assert User02(d).age() == 2
+def test_is_user02() -> None:
+    ddd = date(2020, 1, 23)
+    assert User02(ddd).age() == 2
