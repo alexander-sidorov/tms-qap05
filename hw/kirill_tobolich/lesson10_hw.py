88bdc116be84feefdbf0bfd7da588b3f16d11c77
Tobkirill <tobkirill@gmail,.com>
2022-01-25 19:44:41 +0300

    lesson 10 hw

---
+++ hw/kirill_tobolich/lesson10_hw.py
@@ -0,0 +1,61 @@
+import collections
+from datetime import date
+from typing import Any
+from typing import Collection
+
+from hw.kirill_tobolich.lesson6_hw import get_formatted_birthday
+from hw.kirill_tobolich.lesson6_hw import http_query_parser
+from hw.kirill_tobolich.lesson6_hw import multiply
+from hw.kirill_tobolich.lesson6_hw import palindrome
+
+
+class Palindrome:
+    def __init__(self, string: str):
+        self.string = string
+
+    def __bool__(self) -> bool:
+        if not palindrome(self.string)["data"]:
+            return False
+        return True
+
+
+class User02:  # noqa: SIM119
+    def __init__(self, birthday_date: date):
+        self.age = get_formatted_birthday(birthday_date)["data"]["age"]
+
+
+class HttpQuery03:
+    def __init__(self, string: str):
+        self.string = string
+
+    def __getitem__(self, key: str) -> Any:
+        try:
+            http_query_parser(self.string)["data"][key]
+        except KeyError:
+            return None
+        return http_query_parser(self.string)["data"][key]
+
+
+class Multiplier04:
+    def __init__(self) -> None:
+        self.result = 1
+
+    def add(self, arg: Any) -> Any:
+        self.result *= multiply(arg)["data"]
+        return self
+
+    def get_result(self) -> Any:
+        return self.result
+
+
+class DupCounter05(collections.Counter):
+    def __init__(self, collection: Collection) -> None:
+        self.collection = collection
+        self.result: dict = {}
+
+    def get_dups(self) -> dict:
+        counter_dict = collections.Counter(self.collection)
+        for key, value in counter_dict.items():
+            if value > 1:
+                self.result[key] = value
+        return self.result
---
+++ hw/kirill_tobolich/test_lesson10_hw.py
@@ -0,0 +1,45 @@
+from datetime import date
+
+from hw.kirill_tobolich.lesson10_hw import DupCounter05
+from hw.kirill_tobolich.lesson10_hw import HttpQuery03
+from hw.kirill_tobolich.lesson10_hw import Multiplier04
+from hw.kirill_tobolich.lesson10_hw import Palindrome
+from hw.kirill_tobolich.lesson10_hw import User02
+
+
+def test_class_palindrome() -> None:
+    assert Palindrome("")
+    assert Palindrome(" ")
+    assert Palindrome("x")
+    assert Palindrome("xx")
+    assert Palindrome("abccba")
+    assert not Palindrome("xy") is False
+    assert not Palindrome(123321) is False  # type: ignore
+
+
+def test_class_user02() -> None:
+    d1 = date(2020, 1, 23)
+    d2 = date(year=1987, month=8, day=2)
+    d3 = date(year=1987, month=1, day=2)
+    assert User02(d1).age == 2
+    assert User02(d2).age == 34
+    assert User02(d3).age == 35
+
+
+def test_class_httpquery03() -> None:
+    obj = HttpQuery03("x=1&y=2&y=3")
+    assert obj["x"] == ["1"]
+    assert obj["y"] == ["2", "3"]
+    assert obj["z"] is None
+
+
+def test_class_multiplier04() -> None:
+    obj = Multiplier04()
+    obj.add(2).add(3).add(4)
+    assert obj.get_result() == 24
+
+
+def test_class_dupcounter05() -> None:
+    l1 = [1, 1, 1, 1, 2, 2, 3]
+    c1 = DupCounter05(l1)
+    assert c1.get_dups() == {1: 4, 2: 2}
